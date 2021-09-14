1de180a4543c1b4164e4d33d8ad0bae6ac769abc
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2021-09-14 15:17:44 +0200

    use generic field renderers

--- internal/renderers/publication.go
+++ internal/renderers/publication.go
@@ -10,17 +10,17 @@ import (
 )
 
 type textData struct {
-	Label    string
 	Text     string
-	Tooltip  string
+	Label    string
 	Required bool
+	Tooltip  string
 }
 
 type listData struct {
-	Label    string
 	List     []string
-	Tooltip  string
+	Label    string
 	Required bool
+	Tooltip  string
 }
 
 type Publication struct {
@@ -34,38 +34,20 @@ type Publication struct {
 // Render the "Publication details" section under the Description tab
 
 func (r *Publication) Details(p *models.Publication) template.HTML {
-	return r.partial(fmt.Sprintf("publication/details/_%s", p.Type), p)
+	return r.Partial(fmt.Sprintf("publication/details/_%s", p.Type), p)
 }
 
 func (r *Publication) Conference(p *models.Publication) template.HTML {
-	return r.partial(fmt.Sprintf("publication/conference/_%s", p.Type), p)
+	return r.Partial(fmt.Sprintf("publication/conference/_%s", p.Type), p)
 }
 
 //
 // Render each field
 //
 
-// Render the "Publication type" field
-
-func (r *Publication) Type(p *models.Publication) template.HTML {
-	return r.partial("part/_text", &textData{
-		Label: "Publication type",
-		Text:  p.Type,
-	})
-}
-
-// Render the "DOI" field
-
-func (r *Publication) DOI(p *models.Publication) template.HTML {
-	return r.partial("part/_text", &textData{
-		Label: "DOI",
-		Text:  p.DOI,
-	})
-}
-
 // Render the "ISSN/ISBN" field
 
-func (r *Publication) ISXN(p *models.Publication) template.HTML {
+func (r *Publication) ISXN(p *models.Publication, label string, required bool) template.HTML {
 	var list []string
 	for _, val := range p.ISSN {
 		list = append(list, fmt.Sprintf("ISSN: %s", val))
@@ -74,32 +56,29 @@ func (r *Publication) ISXN(p *models.Publication) template.HTML {
 		list = append(list, fmt.Sprintf("ISBN: %s", val))
 	}
 
-	return r.partial("part/_list", &listData{
+	return r.Partial("part/_list", &listData{
 		Label: "ISSN/ISBN",
 		List:  list,
 	})
 }
 
-// Render the "Title" field
-
-func (r *Publication) Title(p *models.Publication) template.HTML {
-	return r.partial("part/_text", &textData{
-		Label:    "Title",
-		Text:     p.Title,
-		Required: true,
+func (r *Publication) Text(text, label string, required bool) template.HTML {
+	return r.Partial("part/_text", &textData{
+		Label:    label,
+		Text:     text,
+		Required: required,
 	})
 }
 
-// Render the "Alternative Title" field
-
-func (r *Publication) AlternativeTitle(p *models.Publication) template.HTML {
-	return r.partial("part/_list", &listData{
-		Label: "Alternative title",
-		List:  p.AlternativeTitle,
+func (r *Publication) List(list []string, label string, required bool) template.HTML {
+	return r.Partial("part/_list", &listData{
+		Label:    label,
+		List:     list,
+		Required: required,
 	})
 }
 
-func (r *Publication) partial(partial string, vars interface{}) template.HTML {
+func (r *Publication) Partial(partial string, vars interface{}) template.HTML {
 	buf := &bytes.Buffer{}
 	if tmpl := r.Render.TemplateLookup(partial); tmpl != nil {
 		tmpl.Execute(buf, vars)
