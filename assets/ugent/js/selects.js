386345f6c65608633546705a7d0786aab9d7eede
Matthias <matthias@colada.be>
2021-09-16 13:54:55 +0200

    Changed: fixed non-functional select2 jquery extension.

--- assets/ugent/js/selects.js
+++ assets/ugent/js/selects.js
@@ -1,45 +1,51 @@
 // Required modules
-const $ = require('jquery');
-require('select2')();
+import $ from 'jquery';
+require('select2');
 
-function setMultiIconsText(state) {
-  if (!state.id) { return state.text; }
+htmx.on("htmx:afterSwap", function(evt) {
+  if (evt.detail.elt.classList.contains('card-collapsible')) {
+    setTimeout(function() {
+      function setMultiIconsText(state) {
+        if (!state.id) { return state.text; }
 
-  var icons = JSON.parse(state.element.dataset.icon);
+        var icons = JSON.parse(state.element.dataset.icon);
 
-  var iconTemplate = '';
+        var iconTemplate = '';
 
-  icons.forEach((icon) => {
-    iconTemplate+= '<i class="ml-2 if--small text-muted if if-' + icon + '"></i>'
-  })
-  var $multiIcons = $('<div class="d-flex align-items-center">' + state.text + iconTemplate + '</div>');
+        icons.forEach((icon) => {
+          iconTemplate+= '<i class="ml-2 if--small text-muted if if-' + icon + '"></i>'
+        })
+        var $multiIcons = $('<div class="d-flex align-items-center">' + state.text + iconTemplate + '</div>');
 
-  return $multiIcons;
-};
+        return $multiIcons;
+      };
 
-function setIconText (state) {
-  if (!state.id) { return state.text; }
-  var $state = $('<div class="d-flex align-items-center"><i class="mr-2 if if-' + state.element.dataset.icon + '"/></i>' + state.text + '</div>');
-  return $state;
-};
+      function setIconText (state) {
+        if (!state.id) { return state.text; }
+        var $state = $('<div class="d-flex align-items-center"><i class="mr-2 if if-' + state.element.dataset.icon + '"/></i>' + state.text + '</div>');
+        return $state;
+      };
 
-$('select').select2({
-  theme: 'bootstrap4',
-  tags: true,
-  minimumResultsForSearch: Infinity
-});
+      $('select').select2({
+        theme: 'bootstrap4',
+        tags: true,
+        minimumResultsForSearch: Infinity
+      });
 
-$(".select-icon").select2({
-  theme: 'bootstrap4',
-  templateResult: setIconText,
-  templateSelection: setIconText,
-  minimumResultsForSearch: Infinity
-});
+      $(".select-icon").select2({
+        theme: 'bootstrap4',
+        templateResult: setIconText,
+        templateSelection: setIconText,
+        minimumResultsForSearch: Infinity
+      });
 
 
-$(".select-multi-icons").select2({
-  theme: 'bootstrap4',
-  templateResult: setMultiIconsText,
-  templateSelection: setMultiIconsText,
-  minimumResultsForSearch: Infinity
-});
+      $(".select-multi-icons").select2({
+        theme: 'bootstrap4',
+        templateResult: setMultiIconsText,
+        templateSelection: setMultiIconsText,
+        minimumResultsForSearch: Infinity
+      });
+    }, 30)
+  }
+})
