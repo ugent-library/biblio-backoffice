1d04b66929193a1f16eeae9231e7b7da48626d8e
Matthias <matthias@colada.be>
2021-09-29 16:52:26 +0200

    Added: smoot submission flow.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,10 @@ import check from './ui/check.js'
 import bootstrap from './ui/bootstrap.js'
 import multiple from './ui/multiple.js'
 import header from './ui/header.js'
+import submit from './ui/form_submit.js'
 
 bootstrap()
 check()
 multiple()
 header()
+submit()
---
+++ assets/js/ui/form_submit.js
@@ -0,0 +1,59 @@
+import htmx from 'htmx.org';
+// import BSN from "bootstrap.native/dist/bootstrap-native-v4";
+
+export default function() {
+
+    // Compose a spinner element
+    function createSpinner() {
+        const spinner = document.createElement("div")
+        spinner.classList.add('spinner-border')
+
+        const text = document.createElement("span")
+        text.classList.add("sr-only")
+        let cta = document.createTextNode("Loading...")
+        text.appendChild(cta)
+
+        spinner.appendChild(text)
+
+        return spinner
+    }
+
+    // On submit, disable the cancel / save buttons & set the spinner
+    function formSubmit(form) {
+        const submitButton = form.querySelector('.btn-save')
+        const cancelButton = form.querySelector('.btn-cancel')
+
+        submitButton.addEventListener("click", function(evt) {
+            submitButton.setAttribute("disabled", "")
+            cancelButton.setAttribute("disabled", "")
+
+            const spinner = createSpinner()
+            submitButton.after(spinner)
+        })
+    }
+
+    // After submission, auto-dismiss all alerts after 10 seconds.
+    // TODO: if 2 consecutive save actions happen within the 10 second interval,
+    //    the first displayed alert will be destroyed by HTMX, causing the setTimeout
+    //    to trigger a runtime error as it tries to apply a BSN.alert on a non-existing
+    //    element.
+    // function closeAlerts() {
+    //     let alerts = document.querySelectorAll('.alert')
+    //     alerts.forEach((el) => {
+    //         setTimeout(() => {
+    //             let alert = new BSN.Alert(el)
+    //             alert.close()
+    //         }, 10000)
+    //     })
+    // }
+
+    // Init event listeners whenever HTMX swaps in a card-collapsible having a form element.
+    htmx.on("htmx:afterSettle", function(evt) {
+        let item = evt.detail.target.children.item(0)
+        if (item && item.nodeName && (item.nodeName.toLowerCase() == "form")) {
+            formSubmit(item)
+        } else {
+        //    closeAlerts()
+        }
+    });
+}
