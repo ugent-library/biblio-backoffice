f486fcc4dc7df789b1d9e55cbf9ab7b04aa135e4
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2021-10-20 15:02:45 +0200

    refactor context package;add localization infrastructure

---
+++ internal/views/form.go
@@ -0,0 +1,87 @@
+package views
+
+import "github.com/ugent-library/go-web/jsonapi"
+
+type textData struct {
+	Text     string
+	Label    string
+	Required bool
+	Tooltip  string
+}
+
+type listData struct {
+	List     []string
+	Label    string
+	Required bool
+	Tooltip  string
+}
+
+type textFormData struct {
+	Key      string
+	Text     string
+	Label    string
+	Required bool
+	Tooltip  string
+	Cols     int
+	HasError bool
+	Error    jsonapi.Error
+}
+
+type textMultipleFormData struct {
+	Key      string
+	Text     []string
+	Label    string
+	Required bool
+	Tooltip  string
+	Cols     int
+	HasError bool
+	Error    jsonapi.Error
+}
+
+type listFormValues struct {
+	Key      string
+	Value    string
+	Selected bool
+}
+
+type listFormData struct {
+	Key      string
+	Values   []*listFormValues
+	Label    string
+	Required bool
+	Tooltip  string
+	Cols     int
+	HasError bool
+	Error    jsonapi.Error
+}
+
+type listMultipleFormData struct {
+	Key        string
+	Values     map[int][]*listFormValues
+	Vocabulary map[string]string
+	Label      string
+	Required   bool
+	Tooltip    string
+	Cols       int
+	HasError   bool
+	Error      jsonapi.Error
+}
+
+type boolData struct {
+	Value    bool
+	Label    string
+	Required bool
+	Tooltip  string
+}
+
+type CheckboxInput struct {
+	Name     string
+	Value    string
+	Checked  bool
+	Label    string
+	Required bool
+	Tooltip  string
+	Cols     int
+	HasError bool
+	Error    jsonapi.Error
+}
