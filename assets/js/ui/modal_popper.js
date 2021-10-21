49f4ddd17354b03cad42b56be1af4e99f6ef85b7
Matthias <matthias@colada.be>
2021-10-21 14:05:10 +0200

    Fixed: issue where opening action item results in JS error.

--- assets/js/ui/modal_popper.js
+++ assets/js/ui/modal_popper.js
@@ -5,7 +5,7 @@ import Popper from 'popper.js'
 // Bootstrap Native doesn't incorporate Popper.js. We have to wire everything ourselves.
 // See: https://github.com/thednp/bootstrap.native/issues/211
 export default function() {
-    document.querySelectorAll("div.dropdown button").forEach(function(button) {
+    document.querySelectorAll("div.dropdown > button").forEach(function(button) {
         button.addEventListener("click", function(evt) {
             let menu = button.parentElement.children.item(1)
 
