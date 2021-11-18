cacddc634e4ed35e070a96a20e2d1dcf845ed6e5
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2021-11-18 06:51:52 +0100

    fix toast close

--- assets/js/ui/radio_card.js
+++ assets/js/ui/radio_card.js
@@ -1,27 +1,27 @@
 import htmx from 'htmx.org';
 
 export default function () {
-    let addEvents = function(parentEl) {
-        // TODO why is this fired twice for each click?
-        let toggleSelected = function(evt) {
-            let group = evt.currentTarget.closest('.radio-card-group')
-            let cards = group.querySelectorAll('.c-radio-card')
-            cards.forEach(function(card) {
-                card.setAttribute('aria-selected', 'false');
-                card.classList.remove('c-radio-card--selected');
-            })
-            evt.currentTarget.setAttribute('aria-selected', 'true');
-            evt.currentTarget.classList.add('c-radio-card--selected');
-        }
+    let toggleSelected = function(evt) {
+        let group = evt.currentTarget.closest('.radio-card-group')
+        let cards = group.querySelectorAll('.c-radio-card')
+        cards.forEach(function(card) {
+            card.setAttribute('aria-selected', 'false');
+            card.classList.remove('c-radio-card--selected');
+        })
+        evt.currentTarget.setAttribute('aria-selected', 'true');
+        evt.currentTarget.classList.add('c-radio-card--selected');
+    }
 
-        parentEl.querySelectorAll('.radio-card-group .c-radio-card').forEach(card =>
+    let addEvents = function() {
+        document.querySelectorAll('.radio-card-group .c-radio-card').forEach(card =>
             card.addEventListener('click', toggleSelected)
         )
     }
 
-    addEvents(document)
+    addEvents()
 
+    // TODO don't use afterSettle
     htmx.on("htmx:afterSettle", function(evt) {
-        addEvents(evt.detail.target)
+        addEvents()
     });
 }
--- assets/js/ui/toast.js
+++ assets/js/ui/toast.js
@@ -1,22 +1,20 @@
 import htmx from 'htmx.org';
 
 export default function () {
-    let addEvents = function(parentEl) {
-        let dismissToast = function(evt) {
-            evt.currentTarget.closest('.bc-toast').forEach(function(toast) {
-                toast.classList.add('d-none');
-            })
-        }
+    let dismissToast = function(evt) {
+        evt.currentTarget.closest('.bc-toast').classList.add('d-none')
+    }
 
-        parentEl.querySelectorAll('.bc-toast .toast-close').forEach(btn =>
+    let addEvents = function() {
+        document.querySelectorAll('.bc-toast .toast-close').forEach(btn =>
             btn.addEventListener('click', dismissToast)
         )
     }
 
-    addEvents(document)
+    addEvents()
 
     // TODO don't use afterSettle
     htmx.on("htmx:afterSettle", function(evt) {
-        addEvents(evt.detail.target)
+        addEvents()
     });
 }
