f75d8fac78c291959e956aeaaa0f73f2588ff273
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2021-11-18 12:41:01 +0100

    person model and engine GetPerson method

---
+++ internal/models/person.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type PersonDepartment struct {
+	ID string `json:"_id"`
+}
+
+type Person struct {
+	DateCreated *time.Time         `json:"date_created"`
+	DateUpdated *time.Time         `json:"date_updated"`
+	Department  []PersonDepartment `json:"department"`
+	FirstName   string             `json:"first_name"`
+	FullName    string             `json:"full_name"`
+	ID          string             `json:"_id"`
+	LastName    string             `json:"last_name"`
+	ORCID       string             `json:"orcid"`
+}
