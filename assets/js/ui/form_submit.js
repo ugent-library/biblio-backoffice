48dfbafe1fd4c01ac79dc9640796e54ea799d083
Matthias <matthias@colada.be>
2021-12-10 16:45:56 +0100

    Added: extra confirmation / warning for unsaved changes when adding publication / dataset.

--- assets/js/ui/form_submit.js
+++ assets/js/ui/form_submit.js
@@ -1,7 +1,9 @@
 import htmx from 'htmx.org';
+import BSN from "bootstrap.native/dist/bootstrap-native-v4";
 
 export default function() {
 
+    // Disable save/cancel buttons when a htmx request is being processed.
     // See: https://github.com/bigskysoftware/htmx/issues/394
     htmx.on("htmx:afterSettle", function(evt) {
         const buttons = document.querySelectorAll(".btn-save");
@@ -20,4 +22,31 @@ export default function() {
             });
         }
     });
+
+    // Show a warning / confirmation when user navigates away from form with
+    // potential unsaved data.
+    //
+    // @todo: Currently only targets the button in the toolbar. Should become more generic
+    //   and target groups of outgoing links and buttons in the future.
+    htmx.on("htmx:afterSettle", function(evt) {
+        // Find .btn-save buttons on dataset/description and publication/description templates
+        const buttons = document.querySelector(".btn-save");
+        if (buttons !== null) {
+            const publishToBiblio = document.querySelector('.bc-toolbar-right .btn');
+            if (publishToBiblio !== null) {
+                publishToBiblio.setAttribute("data-toggle", "modal");
+                publishToBiblio.setAttribute("data-target", "#confirmation-next-step");
+            }
+        }
+        else {
+            const publishToBiblio = document.querySelector('.bc-toolbar-right .btn');
+            if (publishToBiblio !== null) {
+                publishToBiblio.removeAttribute("data-toggle");
+                publishToBiblio.removeAttribute("data-target");
+            }
+        }
+
+        BSN.initCallback();
+    });
+
 }
