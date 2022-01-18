de6e0658b04ead58568035b83891d92b31c1197c
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2022-01-18 18:05:26 +0100

    autocomplete escape key support

--- assets/js/ui/autocomplete.js
+++ assets/js/ui/autocomplete.js
@@ -2,12 +2,21 @@ import htmx from 'htmx.org';
 
 export default function () {
     let addEvents = function(rootEl) {
+        rootEl.querySelectorAll('.autocomplete').forEach(function(ac) {
+            let target = document.querySelector(ac.dataset.target);
+            target.addEventListener('keydown', function(evt){
+                if (evt.key === "Escape") {
+                    ac.querySelector(".autocomplete-hits").innerHTML = ""
+                }
+            })
+        })
+
         rootEl.querySelectorAll('.autocomplete-hit').forEach(function(hit) {
             hit.addEventListener('click', function () {
                 let target = hit.closest(".autocomplete").dataset.target;
                 let v = hit.dataset.value;
                 document.querySelector(target).value = v;
-                hit.closest(".autocomplete-hits").innerHTML = "";                               
+                hit.closest(".autocomplete-hits").innerHTML = ""                         
             })
         })
     }
