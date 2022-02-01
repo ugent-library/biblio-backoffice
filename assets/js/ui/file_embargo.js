e96dfae17f19725284e5884f54b24ca3de698140
Nicolas Franck <nicolas.franck@ugent.be>
2022-02-01 13:37:47 +0100

    no form attribute; disable inputs, do not clear

--- assets/js/ui/file_embargo.js
+++ assets/js/ui/file_embargo.js
@@ -2,22 +2,28 @@ import htmx from 'htmx.org';
 
 export default function () {
     let addEvents = function() {
-        document.querySelectorAll('#publication-files-modal form input[type=radio][name=access_level]').forEach(function(radio) {
+        document.querySelectorAll('#publication-files-modal input[type=radio][name=access_level]').forEach(function(radio) {
             radio.addEventListener('change', function(evt) {
                 const container = document.getElementById('file-embargo')
+                const embargo   = container.querySelector('input[name=embargo]')
+                const embargo_to= container.querySelector('select[name=embargo_to]')
                 if (this.value === "open_access") {
                     container.classList.add("d-none")
-                    container.querySelector("input[name=embargo]").value = ""
-                    container.querySelector("select[name=embargo_to] option").forEach(opt => opt.selected = false)
+                    embargo.disabled = true
+                    embargo_to.disabled = true
                 } else if (this.value === "local") {
                     container.classList.remove("d-none")
-                    const opt = container.querySelector("select[name=embargo_to] option[value=local]")
+                    const opt = embargo_to.querySelector("option[value=local]")
                     opt.selected = false
                     opt.disabled = true
+                    embargo.disabled = false
+                    embargo_to.disabled = false
                 } else if (this.value === "closed") {
                     container.classList.remove("d-none")
-                    const opt = container.querySelector("select[name=embargo_to] option[value=local]")
+                    const opt = embargo_to.querySelector("option[value=local]")
                     opt.disabled = false
+                    embargo.disabled = false
+                    embargo_to.disabled = false
                 }
             })
         })
