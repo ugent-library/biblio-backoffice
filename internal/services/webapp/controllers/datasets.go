cd92c493eb68e52836a1f0f348fb0c02775ea6a3
nsteenla <nicolas.steenlant@ugent.be>
2022-06-28 11:24:39 +0200

    remove old actions

--- internal/services/webapp/controllers/datasets.go
+++ internal/services/webapp/controllers/datasets.go
@@ -1,34 +1,17 @@
 package controllers
 
 import (
-	"errors"
 	"log"
 	"net/http"
 	"net/url"
 	"time"
 
 	"github.com/ugent-library/biblio-backend/internal/backends"
-	"github.com/ugent-library/biblio-backend/internal/locale"
 	"github.com/ugent-library/biblio-backend/internal/models"
 	"github.com/ugent-library/biblio-backend/internal/services/webapp/context"
 	"github.com/ugent-library/biblio-backend/internal/services/webapp/views"
-	"github.com/ugent-library/biblio-backend/internal/ulid"
-	"github.com/ugent-library/biblio-backend/internal/validation"
 	"github.com/unrolled/render"
 )
-
-// type DatasetListVars struct {
-// 	SearchArgs *models.SearchArgs
-// 	Hits       *models.DatasetHits
-// }
-
-type DatasetAddVars struct {
-	PageTitle        string
-	Step             int
-	Source           string
-	Identifier       string
-	DuplicateDataset *models.Dataset
-}
 
 type Datasets struct {
 	Base
@@ -45,182 +28,6 @@ func NewDatasets(base Base, store backends.Repository, datasetSearchService back
 		datasetSearchService: datasetSearchService,
 		datasetSources:       datasetSources,
 	}
-}
-
-// func (c *Datasets) List(w http.ResponseWriter, r *http.Request) {
-// 	args := models.NewSearchArgs()
-// 	if err := DecodeQuery(args, r.URL.Query()); err != nil {
-// 		log.Println(err)
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	hits, err := c.userDatasets(context.GetUser(r.Context()).ID, args)
-// 	if err != nil {
-// 		log.Println(err)
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	searchURL, _ := c.Router.Get("datasets").URLPath()
-
-// 	c.Render.HTML(w, http.StatusOK, "dataset/list", c.ViewData(r, struct {
-// 		PageTitle  string
-// 		SearchURL  *url.URL
-// 		SearchArgs *models.SearchArgs
-// 		Hits       *models.DatasetHits
-// 	}{
-// 		"Overview - Datasets - Biblio",
-// 		searchURL,
-// 		args,
-// 		hits,
-// 	}))
-// }
-
-func (c *Datasets) Add(w http.ResponseWriter, r *http.Request) {
-	c.Render.HTML(w, http.StatusOK, "dataset/add", c.ViewData(r, DatasetAddVars{
-		PageTitle: "Add - Datasets - Biblio",
-		Step:      1,
-	}))
-}
-
-func (c *Datasets) AddImportConfirm(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	source := r.FormValue("source")
-	identifier := r.FormValue("identifier")
-
-	// check for duplicates
-	if source == "datacite" {
-		args := models.NewSearchArgs().WithFilter("doi", identifier).WithFilter("status", "public")
-		if existing, _ := c.datasetSearchService.Search(args); existing.Total > 0 {
-			c.Render.HTML(w, http.StatusOK, "dataset/add", c.ViewData(r, DatasetAddVars{
-				PageTitle:        "Add - Datasets - Biblio",
-				Step:             1,
-				Source:           source,
-				Identifier:       identifier,
-				DuplicateDataset: existing.Hits[0],
-			}))
-			return
-		}
-	}
-
-	c.AddImport(w, r)
-}
-
-func (c *Datasets) AddImport(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	source := r.FormValue("source")
-	identifier := r.FormValue("identifier")
-	loc := locale.Get(r.Context())
-
-	dataset, err := c.importUserDatasetByIdentifier(context.GetUser(r.Context()).ID, source, identifier)
-
-	if err != nil {
-		log.Println(err)
-		flash := views.Flash{Type: "error"}
-		flash.Message = loc.TS("dataset.single_import", "import_by_id.import_failed")
-
-		c.Render.HTML(w, http.StatusOK, "dataset/add", c.ViewData(r, DatasetAddVars{
-			PageTitle:  "Add - Datasets - Biblio",
-			Step:       1,
-			Source:     source,
-			Identifier: identifier,
-		},
-			flash,
-		))
-		return
-	}
-
-	c.Render.HTML(w, http.StatusOK, "dataset/add_description", c.ViewData(r, struct {
-		PageTitle           string
-		Step                int
-		Dataset             *models.Dataset
-		DatasetPublications []*models.Publication
-		Show                *views.ShowBuilder
-	}{
-		"Add - Datasets - Biblio",
-		2,
-		dataset,
-		nil,
-		views.NewShowBuilder(c.RenderPartial, loc),
-	}))
-}
-
-func (c *Datasets) AddDescription(w http.ResponseWriter, r *http.Request) {
-	dataset := context.GetDataset(r.Context())
-
-	c.Render.HTML(w, http.StatusOK, "dataset/add_description", c.ViewData(r, struct {
-		PageTitle           string
-		Step                int
-		Dataset             *models.Dataset
-		DatasetPublications []*models.Publication
-		Show                *views.ShowBuilder
-	}{
-		"Add - Datasets - Biblio",
-		2,
-		dataset,
-		nil,
-		views.NewShowBuilder(c.RenderPartial, locale.Get(r.Context())),
-	}))
-}
-
-func (c *Datasets) AddConfirm(w http.ResponseWriter, r *http.Request) {
-	dataset := context.GetDataset(r.Context())
-
-	c.Render.HTML(w, http.StatusOK, "dataset/add_confirm", c.ViewData(r, struct {
-		PageTitle string
-		Step      int
-		Dataset   *models.Dataset
-	}{
-		"Add - Datasets - Biblio",
-		3,
-		dataset,
-	}))
-}
-
-func (c *Datasets) AddPublish(w http.ResponseWriter, r *http.Request) {
-	dataset := context.GetDataset(r.Context())
-
-	dataset.Status = "public"
-	savedDataset := dataset.Clone()
-	err := c.store.SaveDataset(dataset)
-	if err != nil {
-
-		/*
-		   TODO: return to dataset - add_confirm with flash in session instead of rendering this in the wrong path
-		   We only use one error, as publishing can only fail on attribute title
-		*/
-		if e, ok := err.(validation.Errors); ok {
-			c.Render.HTML(w, http.StatusOK, "dataset/add_confirm", c.ViewData(r, struct {
-				PageTitle string
-				Step      int
-				Dataset   *models.Dataset
-			}{
-				"Add - Datasets - Biblio",
-				3,
-				dataset,
-			},
-				views.Flash{Type: "error", Message: e.Error()},
-			))
-			return
-		}
-
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	c.Render.HTML(w, http.StatusOK, "dataset/add_finish", c.ViewData(r, struct {
-		PageTitle string
-		Step      int
-		Dataset   *models.Dataset
-	}{
-		"Add - Datasets - Biblio",
-		4,
-		savedDataset,
-	}))
 }
 
 func (c *Datasets) ConfirmDelete(w http.ResponseWriter, r *http.Request) {
@@ -302,25 +109,4 @@ func (c *Datasets) userDatasets(userID string, args *models.SearchArgs) (*models
 	searcher = searcher.IncludeFacets(true)
 
 	return searcher.Search(args)
-}
-
-func (c *Datasets) importUserDatasetByIdentifier(userID, source, identifier string) (*models.Dataset, error) {
-	s, ok := c.datasetSources[source]
-	if !ok {
-		return nil, errors.New("unknown dataset source")
-	}
-	d, err := s.GetDataset(identifier)
-	if err != nil {
-		return nil, err
-	}
-	d.ID = ulid.MustGenerate()
-	d.CreatorID = userID
-	d.UserID = userID
-	d.Status = "private"
-
-	if err = c.store.SaveDataset(d); err != nil {
-		return nil, err
-	}
-
-	return d, nil
 }
