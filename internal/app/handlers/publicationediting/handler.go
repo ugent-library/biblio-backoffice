c64b44c2adc7bf31307f747002072b529c322317
Nicolas Franck <nicolas.franck@ugent.be>
2022-06-29 08:49:54 +0200

    add publicationediting/handler.go

---
+++ internal/app/handlers/publicationediting/handler.go
@@ -0,0 +1,53 @@
+package publicationediting
+
+import (
+	"net/http"
+
+	"github.com/ugent-library/biblio-backend/internal/app/handlers"
+	"github.com/ugent-library/biblio-backend/internal/backends"
+	"github.com/ugent-library/biblio-backend/internal/bind"
+	"github.com/ugent-library/biblio-backend/internal/models"
+	"github.com/ugent-library/biblio-backend/internal/render"
+)
+
+type Handler struct {
+	handlers.BaseHandler
+	Repository                backends.Repository
+	ProjectSearchService      backends.ProjectSearchService
+	ProjectService            backends.ProjectService
+	PersonService             backends.PersonService
+	PersonSearchService       backends.PersonSearchService
+	OrganizationSearchService backends.OrganizationSearchService
+	OrganizationService       backends.OrganizationService
+	PublicationSearchService  backends.PublicationSearchService
+}
+
+type Context struct {
+	handlers.BaseContext
+	Publication *models.Publication
+}
+
+func (h *Handler) Wrap(fn func(http.ResponseWriter, *http.Request, Context)) http.HandlerFunc {
+	return h.BaseHandler.Wrap(func(w http.ResponseWriter, r *http.Request, ctx handlers.BaseContext) {
+		if ctx.User == nil {
+			render.Unauthorized(w, r)
+			return
+		}
+
+		pub, err := h.Repository.GetPublication(bind.PathValues(r).Get("id"))
+		if err != nil {
+			render.InternalServerError(w, r, err)
+			return
+		}
+
+		if !ctx.User.CanEditPublication(pub) {
+			render.Forbidden(w, r)
+			return
+		}
+
+		fn(w, r, Context{
+			BaseContext: ctx,
+			Publication: pub,
+		})
+	})
+}
