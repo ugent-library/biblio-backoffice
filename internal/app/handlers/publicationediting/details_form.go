55b464d4e535f7536f232b32a428fb3bf3c8ca6d
nsteenla <nicolas.steenlant@ugent.be>
2022-09-22 12:16:49 +0200

    update type handler

--- internal/app/handlers/publicationediting/details_form.go
+++ internal/app/handlers/publicationediting/details_form.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ugent-library/biblio-backend/internal/validation"
 )
 
-func detailsForm(user *models.User, l *locale.Locale, publication *models.Publication, errors validation.Errors) *form.Form {
+func detailsForm(user *models.User, l *locale.Locale, p *models.Publication, errors validation.Errors) *form.Form {
 	f := form.New().
 		WithTheme("default").
 		WithErrors(localize.ValidationErrors(l, errors))
@@ -23,33 +23,33 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		Name:     "type",
 		Label:    l.T("builder.type"),
 		Options:  localize.VocabularySelectOptions(l, "publication_types"),
-		Value:    publication.Type,
+		Value:    p.Type,
 		Cols:     3,
 		Help:     l.T("builder.type.help"),
 		Vars: struct {
 			Publication *models.Publication
 		}{
-			Publication: publication,
+			Publication: p,
 		},
 	})
 
-	if publication.UsesJournalArticleType() {
+	if p.UsesJournalArticleType() {
 		section1 = append(section1, &form.Select{
 			Name:        "journal_article_type",
 			Label:       l.T("builder.journal_article_type"),
 			Options:     localize.VocabularySelectOptions(l, "journal_article_types"),
 			EmptyOption: true,
-			Value:       publication.JournalArticleType,
+			Value:       p.JournalArticleType,
 			Cols:        3,
 			Error:       localize.ValidationErrorAt(l, errors, "/journal_article_type"),
 		})
 	}
 
-	if publication.UsesConferenceType() {
+	if p.UsesConferenceType() {
 		section1 = append(section1, &form.Select{
 			Name:        "conference_type",
 			Label:       l.T("builder.conference_type"),
-			Value:       publication.ConferenceType,
+			Value:       p.ConferenceType,
 			Options:     localize.VocabularySelectOptions(l, "conference_types"),
 			EmptyOption: true,
 			Cols:        3,
@@ -57,11 +57,11 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		})
 	}
 
-	if publication.UsesMiscellaneousType() {
+	if p.UsesMiscellaneousType() {
 		section1 = append(section1, &form.Select{
 			Name:        "miscellaneous_type",
 			Label:       l.T("builder.miscellaneous_type"),
-			Value:       publication.MiscellaneousType,
+			Value:       p.MiscellaneousType,
 			EmptyOption: true,
 			Options:     localize.VocabularySelectOptions(l, "miscellaneous_types"),
 			Cols:        3,
@@ -69,11 +69,11 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		})
 	}
 
-	if publication.UsesDOI() {
+	if p.UsesDOI() {
 		section1 = append(section1, &form.Text{
 			Name:  "doi",
 			Label: l.T("builder.doi"),
-			Value: publication.DOI,
+			Value: p.DOI,
 			Cols:  9,
 			Help:  l.T("builder.doi.help"),
 			Error: localize.ValidationErrorAt(l, errors, "/doi"),
@@ -82,7 +82,7 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 
 	section1 = append(section1, &display.Text{
 		Label:   l.T("builder.classification"),
-		Value:   l.TS("publication_classifications", publication.Classification),
+		Value:   l.TS("publication_classifications", p.Classification),
 		Tooltip: l.T("tooltip.publication.classification"),
 	})
 
@@ -92,43 +92,43 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 
 	section2 := []form.Field{}
 
-	if publication.UsesTitle() {
+	if p.UsesTitle() {
 		section2 = append(section2, &form.Text{
 			Name:     "title",
 			Label:    l.T("builder.title"),
-			Value:    publication.Title,
+			Value:    p.Title,
 			Cols:     9,
 			Error:    localize.ValidationErrorAt(l, errors, "/title"),
 			Required: true,
 		})
 	}
 
-	if publication.UsesAlternativeTitle() {
+	if p.UsesAlternativeTitle() {
 		section2 = append(section2, &form.TextRepeat{
 			Name:   "alternative_title",
 			Label:  l.T("builder.alternative_title"),
-			Values: publication.AlternativeTitle,
+			Values: p.AlternativeTitle,
 			Cols:   9,
 			Error:  localize.ValidationErrorAt(l, errors, "/alternative_title"),
 		})
 	}
 
-	if publication.UsesPublication() {
+	if p.UsesPublication() {
 		section2 = append(section2, &form.Text{
 			Name:     "publication",
-			Label:    l.T(fmt.Sprintf("builder.%s.publication", publication.Type)),
-			Value:    publication.Publication,
+			Label:    l.T(fmt.Sprintf("builder.%s.publication", p.Type)),
+			Value:    p.Publication,
 			Cols:     9,
 			Required: true,
 			Error:    localize.ValidationErrorAt(l, errors, "/publication"),
 		})
 	}
 
-	if publication.UsesPublicationAbbreviation() {
+	if p.UsesPublicationAbbreviation() {
 		section2 = append(section2, &form.Text{
 			Name:  "publication_abbreviation",
-			Label: l.T(fmt.Sprintf("builder.%s.publication_abbreviation", publication.Type)),
-			Value: publication.PublicationAbbreviation,
+			Label: l.T(fmt.Sprintf("builder.%s.publication_abbreviation", p.Type)),
+			Value: p.PublicationAbbreviation,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/publication_abbreviation"),
 		})
@@ -140,25 +140,25 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 
 	section3 := []form.Field{}
 
-	if publication.UsesLanguage() {
+	if p.UsesLanguage() {
 		section3 = append(section3, &form.SelectRepeat{
 			Name:        "language",
 			Label:       l.T("builder.language"),
 			Options:     localize.LanguageSelectOptions(l),
-			Values:      publication.Language,
+			Values:      p.Language,
 			EmptyOption: true,
 			Cols:        9,
 			Error:       localize.ValidationErrorAt(l, errors, "/language"),
 		})
 	}
 
-	if publication.UsesPublicationStatus() {
+	if p.UsesPublicationStatus() {
 		section3 = append(section3, &form.Select{
 			Name:        "publication_status",
 			Label:       l.T("builder.publication_status"),
 			EmptyOption: true,
 			Options:     localize.VocabularySelectOptions(l, "publication_publishing_statuses"),
-			Value:       publication.PublicationStatus,
+			Value:       p.PublicationStatus,
 			Cols:        3,
 			Error:       localize.ValidationErrorAt(l, errors, "/publication_status"),
 		})
@@ -168,16 +168,16 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		Name:    "extern",
 		Label:   l.T("builder.extern"),
 		Value:   "true",
-		Checked: publication.Extern,
+		Checked: p.Extern,
 		Cols:    9,
 		Error:   localize.ValidationErrorAt(l, errors, "/extern"),
 	})
 
-	if publication.UsesYear() {
+	if p.UsesYear() {
 		section3 = append(section3, &form.Text{
 			Name:     "year",
 			Label:    l.T("builder.year"),
-			Value:    publication.Year,
+			Value:    p.Year,
 			Required: true,
 			Cols:     3,
 			Help:     l.T("builder.year.help"),
@@ -185,19 +185,19 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		})
 	}
 
-	if publication.UsesPublisher() {
+	if p.UsesPublisher() {
 		section3 = append(section3,
 			&form.Text{
 				Name:  "place_of_publication",
 				Label: l.T("builder.place_of_publication"),
-				Value: publication.PlaceOfPublication,
+				Value: p.PlaceOfPublication,
 				Cols:  9,
 				Error: localize.ValidationErrorAt(l, errors, "/place_of_publication"),
 			},
 			&form.Text{
 				Name:  "publisher",
 				Label: l.T("builder.publisher"),
-				Value: publication.Publisher,
+				Value: p.Publisher,
 				Cols:  9,
 				Error: localize.ValidationErrorAt(l, errors, "/publisher"),
 			})
@@ -209,97 +209,97 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 
 	section4 := []form.Field{}
 
-	if publication.UsesSeriesTitle() {
+	if p.UsesSeriesTitle() {
 		section4 = append(section4, &form.Text{
 			Name:  "series_title",
 			Label: l.T("builder.series_title"),
-			Value: publication.SeriesTitle,
+			Value: p.SeriesTitle,
 			Cols:  9,
 			Error: localize.ValidationErrorAt(l, errors, "/series_title"),
 		})
 	}
 
-	if publication.UsesVolume() {
+	if p.UsesVolume() {
 		section4 = append(section4, &form.Text{
 			Name:  "volume",
 			Label: l.T("builder.volume"),
-			Value: publication.Volume,
+			Value: p.Volume,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/volume"),
 		})
 	}
 
-	if publication.UsesIssue() {
+	if p.UsesIssue() {
 		section4 = append(section4,
 			&form.Text{
 				Name:  "issue",
 				Label: l.T("builder.issue"),
-				Value: publication.Issue,
+				Value: p.Issue,
 				Cols:  3,
 				Error: localize.ValidationErrorAt(l, errors, "/issue"),
 			}, &form.Text{
 				Name:  "issue_title",
 				Label: l.T("builder.issue_title"),
-				Value: publication.IssueTitle,
+				Value: p.IssueTitle,
 				Cols:  9,
 				Error: localize.ValidationErrorAt(l, errors, "/issue_title"),
 			})
 	}
 
-	if publication.UsesEdition() {
+	if p.UsesEdition() {
 		section4 = append(section4, &form.Text{
 			Name:  "edition",
 			Label: l.T("builder.edition"),
-			Value: publication.Edition,
+			Value: p.Edition,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/edition"),
 		})
 	}
 
-	if publication.UsesPage() {
+	if p.UsesPage() {
 		section4 = append(section4,
 			&form.Text{
 				Name:  "page_first",
 				Label: l.T("builder.page_first"),
-				Value: publication.PageFirst,
+				Value: p.PageFirst,
 				Cols:  3,
 				Error: localize.ValidationErrorAt(l, errors, "/page_first"),
 			},
 			&form.Text{
 				Name:  "page_last",
 				Label: l.T("builder.page_last"),
-				Value: publication.PageLast,
+				Value: p.PageLast,
 				Cols:  3,
 				Error: localize.ValidationErrorAt(l, errors, "/page_last"),
 			},
 		)
 	}
 
-	if publication.UsesPageCount() {
+	if p.UsesPageCount() {
 		section4 = append(section4, &form.Text{
 			Name:  "page_count",
 			Label: l.T("builder.page_count"),
-			Value: publication.PageCount,
+			Value: p.PageCount,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/page_count"),
 		})
 	}
 
-	if publication.UsesArticleNumber() {
+	if p.UsesArticleNumber() {
 		section4 = append(section4, &form.Text{
 			Name:  "article_number",
 			Label: l.T("builder.article_number"),
-			Value: publication.ArticleNumber,
+			Value: p.ArticleNumber,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/article_number"),
 		})
 	}
 
-	if publication.UsesReportNumber() {
+	if p.UsesReportNumber() {
 		section4 = append(section4, &form.Text{
 			Name:  "report_number",
 			Label: l.T("builder.report_number"),
-			Value: publication.ReportNumber,
+			Value: p.ReportNumber,
 			Cols:  3,
 			Error: localize.ValidationErrorAt(l, errors, "/report_number"),
 		})
@@ -309,12 +309,12 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		f.AddSection(section4...)
 	}
 
-	if publication.UsesDefense() {
+	if p.UsesDefense() {
 		f.AddSection(
 			&form.Text{
 				Name:     "defense_date",
 				Label:    l.T("builder.defense_date"),
-				Value:    publication.DefenseDate,
+				Value:    p.DefenseDate,
 				Required: true,
 				Cols:     3,
 				Help:     l.T("builder.defense_date.help"),
@@ -323,7 +323,7 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 			&form.Text{
 				Name:     "defense_time",
 				Label:    l.T("builder.defense_time"),
-				Value:    publication.DefenseTime,
+				Value:    p.DefenseTime,
 				Required: true,
 				Cols:     3,
 				Help:     l.T("builder.defense_time.help"),
@@ -332,7 +332,7 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 			&form.Text{
 				Name:     "defense_place",
 				Label:    l.T("builder.defense_place"),
-				Value:    publication.DefensePlace,
+				Value:    p.DefensePlace,
 				Required: true,
 				Cols:     3,
 				Error:    localize.ValidationErrorAt(l, errors, "/defense_place"),
@@ -340,35 +340,35 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		)
 	}
 
-	if publication.UsesConfirmations() {
+	if p.UsesConfirmations() {
 		confirmationOptions := localize.VocabularySelectOptions(l, "confirmations")
 
 		f.AddSection(
 			&form.RadioButtonGroup{
 				Name:    "has_confidential_data",
 				Label:   l.T("builder.has_confidential_data"),
-				Value:   publication.HasConfidentialData,
+				Value:   p.HasConfidentialData,
 				Options: confirmationOptions,
 				Error:   localize.ValidationErrorAt(l, errors, "/has_confidential_data"),
 			},
 			&form.RadioButtonGroup{
 				Name:    "has_patent_application",
 				Label:   l.T("builder.has_patent_application"),
-				Value:   publication.HasPatentApplication,
+				Value:   p.HasPatentApplication,
 				Options: confirmationOptions,
 				Error:   localize.ValidationErrorAt(l, errors, "/has_patent_application"),
 			},
 			&form.RadioButtonGroup{
 				Name:    "has_publications_planned",
 				Label:   l.T("builder.has_publications_planned"),
-				Value:   publication.HasPublicationsPlanned,
+				Value:   p.HasPublicationsPlanned,
 				Options: confirmationOptions,
 				Error:   localize.ValidationErrorAt(l, errors, "/has_publications_planned"),
 			},
 			&form.RadioButtonGroup{
 				Name:    "has_published_material",
 				Label:   l.T("builder.has_published_material"),
-				Value:   publication.HasPublishedMaterial,
+				Value:   p.HasPublishedMaterial,
 				Options: confirmationOptions,
 				Error:   localize.ValidationErrorAt(l, errors, "/has_published_material"),
 			},
@@ -377,18 +377,18 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 
 	section5 := []form.Field{}
 
-	if publication.UsesWOS() {
+	if p.UsesWOS() {
 		if user.CanCuratePublications() {
 			section5 = append(section5, &form.Text{
 				Name:    "wos_type",
 				Label:   l.T("builder.wos_type"),
-				Value:   publication.WOSType,
+				Value:   p.WOSType,
 				Tooltip: l.T("tooltip.publication.wos_type"),
 			})
 		} else {
 			section5 = append(section5, &display.Text{
 				Label:   l.T("builder.wos_type"),
-				Value:   publication.WOSType,
+				Value:   p.WOSType,
 				Tooltip: l.T("tooltip.publication.wos_type"),
 			})
 		}
@@ -396,19 +396,19 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 		section5 = append(section5, &form.Text{
 			Name:  "wos_id",
 			Label: l.T("builder.wos_id"),
-			Value: publication.WOSID,
+			Value: p.WOSID,
 			Cols:  3,
 			Help:  l.T("builder.wos_id.help"),
 			Error: localize.ValidationErrorAt(l, errors, "/wos_id"),
 		})
 	}
 
-	if publication.UsesISSN() {
+	if p.UsesISSN() {
 		section5 = append(section5,
 			&form.TextRepeat{
 				Name:   "issn",
 				Label:  l.T("builder.issn"),
-				Values: publication.ISSN,
+				Values: p.ISSN,
 				Cols:   3,
 				Help:   l.T("builder.issn.help"),
 				Error:  localize.ValidationErrorAt(l, errors, "/issn"),
@@ -416,19 +416,19 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 			&form.TextRepeat{
 				Name:   "eissn",
 				Label:  l.T("builder.eissn"),
-				Values: publication.EISSN,
+				Values: p.EISSN,
 				Cols:   3,
 				Help:   l.T("builder.eissn.help"),
 				Error:  localize.ValidationErrorAt(l, errors, "/eissn"),
 			})
 	}
 
-	if publication.UsesISBN() {
+	if p.UsesISBN() {
 		section5 = append(section5,
 			&form.TextRepeat{
 				Name:   "isbn",
 				Label:  l.T("builder.isbn"),
-				Values: publication.ISBN,
+				Values: p.ISBN,
 				Cols:   3,
 				Help:   l.T("builder.isbn.help"),
 				Error:  localize.ValidationErrorAt(l, errors, "/isbn"),
@@ -436,40 +436,40 @@ func detailsForm(user *models.User, l *locale.Locale, publication *models.Public
 			&form.TextRepeat{
 				Name:   "eisbn",
 				Label:  l.T("builder.eisbn"),
-				Values: publication.EISBN,
+				Values: p.EISBN,
 				Cols:   3,
 				Help:   l.T("builder.eisbn.help"),
 				Error:  localize.ValidationErrorAt(l, errors, "/eisbn"),
 			})
 	}
 
-	if publication.UsesPubMedID() {
+	if p.UsesPubMedID() {
 		section5 = append(section5, &form.Text{
 			Name:  "pubmed_id",
 			Label: l.T("builder.pubmed_id"),
-			Value: publication.PubMedID,
+			Value: p.PubMedID,
 			Cols:  3,
 			Help:  l.T("builder.pubmed_id.help"),
 			Error: localize.ValidationErrorAt(l, errors, "/pubmed_id"),
 		})
 	}
 
-	if publication.UsesArxivID() {
+	if p.UsesArxivID() {
 		section5 = append(section5, &form.Text{
 			Name:  "arxiv_id",
 			Label: l.T("builder.arxiv_id"),
-			Value: publication.ArxivID,
+			Value: p.ArxivID,
 			Cols:  3,
 			Help:  l.T("builder.arxiv_id.help"),
 			Error: localize.ValidationErrorAt(l, errors, "/arxiv_id"),
 		})
 	}
 
-	if publication.UsesESCIID() {
+	if p.UsesESCIID() {
 		section5 = append(section5, &form.Text{
 			Name:  "esci_id",
 			Label: l.T("builder.esci_id"),
-			Value: publication.ESCIID,
+			Value: p.ESCIID,
 			Cols:  3,
 			Help:  l.T("builder.esci_id.help"),
 			Error: localize.ValidationErrorAt(l, errors, "/esci_id"),
