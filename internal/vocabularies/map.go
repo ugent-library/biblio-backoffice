fda8cdfd9a88a2c22e1b0cb041d4f9ac0b053063
Matthias <matthias@colada.be>
2022-10-12 15:29:32 +0200

    Fixed #687: remove irrelevant filters from datasets view

--- internal/vocabularies/map.go
+++ internal/vocabularies/map.go
@@ -43,18 +43,12 @@ var Map = map[string][]string{
 	"dataset_facets": {
 		"status",
 		"faculty",
-		"type",
-		"wos_type",
-		"classification",
 		"created_since",
 		"updated_since",
 	},
 	"dataset_curation_facets": {
 		"status",
 		"faculty",
-		"type",
-		"wos_type",
-		"classification",
 		"reviewer_tags",
 		"created_since",
 		"updated_since",
