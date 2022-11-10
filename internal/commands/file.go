974048ea2e9de1be813c2904e140cbc85db8389a
nsteenla <nicolas.steenlant@ugent.be>
2022-11-10 09:58:54 +0100

    fix skip file import

--- internal/commands/file.go
+++ internal/commands/file.go
@@ -179,8 +179,7 @@ var fileImportManyCmd = &cobra.Command{
 			}
 
 			// skip files that are already in the store
-			if _, err := os.Stat(fs.FilePath(importFile.Sha256)); err != nil {
-				fmt.Fprintf(os.Stderr, "skipping file %s\n", importFile.File)
+			if _, err := os.Stat(fs.FilePath(importFile.Sha256)); err == nil {
 				// <file-id> <old-path>
 				fmt.Printf("%s %s\n", importFile.Sha256, importFile.File)
 				lineNo++
