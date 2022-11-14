d69f73bccc264bd6171b1e1d786e8049f65f35a8
Nicolas Franck <nicolas.franck@ugent.be>
2022-11-14 16:05:08 +0100

    add extra jsonb indexes for frontoffice handler

--- etc/snapstore/migrations/001_create_datasets.sql
+++ etc/snapstore/migrations/001_create_datasets.sql
@@ -10,6 +10,8 @@ create index datasets_snapshot_id_idx on datasets(snapshot_id);
 create index datasets_id_idx on datasets(id);
 create index datasets_date_from_idx on datasets(date_from);
 create index datasets_date_until_idx on datasets(date_until);
+create index if not exists datasets_date_created_idx on datasets((data->>'date_created'));
+create index if not exists datasets_date_updated_idx on datasets((data->>'date_updated'));
 
 ---- create above / drop below ----
 
