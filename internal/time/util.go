f3f3f4a93649473182bad838df0e208aa0a848bc
Nicolas Franck <nicolas.franck@ugent.be>
2022-12-14 14:20:51 +0100

    make incremental for frontoffice better

--- internal/time/util.go
+++ internal/time/util.go
@@ -10,3 +10,11 @@ const TimeFormatUTC = "2006-01-02T15:04:05.999Z"
 func FormatTimeUTC(t *time.Time) string {
 	return t.UTC().Format(TimeFormatUTC)
 }
+
+func ParseTimeUTC(ds string) (*time.Time, error) {
+	t, e := time.Parse(TimeFormatUTC, ds)
+	if e != nil {
+		return nil, e
+	}
+	return &t, nil
+}
