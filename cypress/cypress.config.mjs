ab5c1331f960a637478fac5beefc612b2eece8f0
Koen Verheyen <verheyen.koen@gmail.com>
2023-01-04 12:02:34 +0100

    Set-up Cypress Dashboard recording

--- cypress/cypress.config.mjs
+++ cypress/cypress.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 import dotenvPlugin from 'cypress-dotenv'
 
 export default defineConfig({
+  projectId: 'mjg74d',
+
   e2e: {
     baseUrl: 'https://backoffice.bibliotest.ugent.be',
     experimentalStudio: true,
