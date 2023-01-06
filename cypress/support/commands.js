a56300b8536548cbb765606dc29f432762aa8e3d
Koen Verheyen <verheyen.koen@gmail.com>
2023-01-06 11:04:35 +0100

    Refactored login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,37 +1,41 @@
-Cypress.Commands.add('login', (username, password) => {
-  cy.session(
-    username,
-    () => {
-      cy.request('/login')
-        .then(response => {
-          const action = response.body.match(/action\=\"(.*)\" /)[1]
+Cypress.Commands.addAll(
+  {
+    cacheAcrossSpecs: true,
+  },
+  {
+    login(username, password) {
+      cy.session(username, () => {
+        // WARNING: Whenever you change the code of the session setup, Cypress will throw an error:
+        //
 
-          return action.replace(/&amp;/g, '&')
-        })
-        .then(actionUrl =>
-          cy.request({
-            method: 'POST',
-            url: actionUrl,
-            form: true,
+        cy.request('/login')
+          .then(response => {
+            const action = response.body.match(/action\=\"(.*)\" /)[1]
 
-            body: {
-              username,
-              password,
-            },
-
-            // Make sure we redirect back and get the cookie we need
-            followRedirect: true,
-
-            // Make sure we don't leak passwords in the Cypress log
-            log: false,
+            return action.replace(/&amp;/g, '&')
           })
-        )
+          .then(actionUrl =>
+            cy.request({
+              method: 'POST',
+              url: actionUrl,
+              form: true,
+
+              body: {
+                username,
+                password,
+              },
+
+              // Make sure we redirect back and get the cookie we need
+              followRedirect: true,
+
+              // Make sure we don't leak passwords in the Cypress log
+              log: false,
+            })
+          )
+      })
     },
-    {
-      cacheAcrossSpecs: true,
-    }
-  )
-})
+  }
+)
 
 Cypress.Commands.add('loginAsResearcher', () => {
   cy.login(Cypress.env('RESEARCHER_USER_NAME'), Cypress.env('RESEARCHER_USER_PASSWORD'))
