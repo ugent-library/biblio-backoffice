30ad64c0fbfbedf086f76571e4a44a2610df8021
Koen Verheyen <verheyen.koen@gmail.com>
2023-01-06 11:40:46 +0100

    Refactored commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,16 @@
-Cypress.Commands.addAll(
-  {
-    cacheAcrossSpecs: true,
-  },
-  {
-    login(username, password) {
-      cy.session(username, () => {
-        // WARNING: Whenever you change the code of the session setup, Cypress will throw an error:
-        //
+Cypress.Commands.addAll({
+  login(username, password) {
+    // WARNING: Whenever you change the code of the session setup, Cypress will throw an error:
+    //   This session already exists. You may not create a new session with a previously used identifier.
+    //   If you want to create a new session with a different setup function, please call cy.session() with
+    //   a unique identifier other than...
+    //
+    // Temporarily uncomment the following line to clear the sessions if this happens
+    // Cypress.session.clearAllSavedSessions()
 
+    cy.session(
+      username,
+      () => {
         cy.request('/login')
           .then(response => {
             const action = response.body.match(/action\=\"(.*)\" /)[1]
@@ -32,15 +35,18 @@ Cypress.Commands.addAll(
               log: false,
             })
           )
-      })
-    },
-  }
-)
+      },
+      {
+        cacheAcrossSpecs: true,
+      }
+    )
+  },
 
-Cypress.Commands.add('loginAsResearcher', () => {
-  cy.login(Cypress.env('RESEARCHER_USER_NAME'), Cypress.env('RESEARCHER_USER_PASSWORD'))
-})
+  loginAsResearcher() {
+    cy.login(Cypress.env('RESEARCHER_USER_NAME'), Cypress.env('RESEARCHER_USER_PASSWORD'))
+  },
 
-Cypress.Commands.add('loginAsLibrarian', () => {
-  cy.login(Cypress.env('LIBRARIAN_USER_NAME'), Cypress.env('LIBRARIAN_USER_PASSWORD'))
+  loginAsLibrarian() {
+    cy.login(Cypress.env('LIBRARIAN_USER_NAME'), Cypress.env('LIBRARIAN_USER_PASSWORD'))
+  },
 })
