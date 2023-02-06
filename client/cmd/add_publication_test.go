25ca119d2753ba1b5e6ad7a5d8db9a53811ef677
Matthias <matthias@colada.be>
2023-02-06 15:20:49 +0100

    Added: first integration test for add publication.

--- client/cmd/add_publication_test.go
+++ client/cmd/add_publication_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
@@ -14,38 +15,163 @@ import (
 
 type AddPublicationSuite struct {
 	suite.Suite
-	cmd *cobra.Command
-	buf *bytes.Buffer
+	addCmd *cobra.Command
+	getCmd *cobra.Command
+	buf    *bytes.Buffer
 }
 
 func (s *AddPublicationSuite) SetupTest() {
 	addPublications := AddPublicationsCmd{}
-	s.cmd = addPublications.Command()
+	s.addCmd = addPublications.Command()
+
+	getPublication := GetPublicationCmd{}
+	s.getCmd = getPublication.Command()
 
 	viper.Set("port", "3999")
 	viper.Set("insecure", true)
 
 	s.buf = bytes.NewBufferString("")
-	s.cmd.SetOut(s.buf)
+	s.addCmd.SetOut(s.buf)
+	s.getCmd.SetOut(s.buf)
 }
 
-func (s *AddPublicationSuite) TestAddPublication() {
+// Test empty input
+// Test non-JSONL input
+// Test empty JSONL object
+
+// Create new valid record
+
+// Test whether all fields are present
+
+// Update existing valid record
+
+// Test whether all fields are present
+
+// Validation: type field is required
+// Validation: type is invalid
+// Validation: classification is required
+// Validation: classification is invalid
+// Validation: status is required
+// Validation: status is invalid
+// Validation: title field is required
+// Validation: year field is required
+// Validation: year field is invalid
+// Validation: language code is invalid
+// Validation: abstract => ID is required
+// Validation: abstract => lang is required
+// Validation: abstract => lang is invalid
+// Validation: abstraxt => text is required
+// Validation: laysummary => ID is required
+// Validation: laysummary => lang is required
+// Validation: laysummary => lang is invalid
+// Validation: laysummary => text is required
+// Validation: if status = public => at least 1 author
+// Validation: if status = public, !legacy, usesAuthor(), !extern => at least 1 ugent author
+// Validation: if status = public, usesEditor, !usesAuthor => at least 1 editor
+// Validation: if status = public, usesEditor, !usesAuthor, !extern => at least 1 ugent editor
+// Validation: if status = public, !legacy, usesSupervisor => at least 1 supervisor
+// Validation: author => ID is required
+// Validation: author => first name is required
+// Validation: author => last name is required
+// Validation: author => credit role is required
+// Validation: author => credit role is invalid
+// Validation: editor => ID is required
+// Validation: editor => first name is required
+// Validation: editor => last name is required
+// Validation: editor => credit role is required
+// Validation: editor => credit role is invalid
+// Validation: supervisor => ID is required
+// Validation: supervisor => first name is required
+// Validation: supervisor => last name is required
+// Validation: supervisor => credit role is required
+// Validation: supervisor => credit role is invalid
+// Validation: project => ID is required
+// Validation: department => ID is required
+// Validation: relatedDataset => ID is required
+// Validation: file ........
+// Validation: link => invalid relation
+
+// Specifics
+
+// Dissertation
+// JournalArticle
+// Miscellaneous
+// book
+// book_chapter
+// conference
+// book_editor
+// issue_editor
+
+// Test if all fields return properly
+
+// Test validation
+func (s *AddPublicationSuite) TestAddAndGetValidPublication() {
 	t := s.T()
 
-	in := strings.NewReader("{}\n")
-	s.cmd.SetIn(in)
+	// Add a publication
+	// addPublications := AddPublicationsCmd{}
+	// addCmd := addPublications.Command()
+	// addCmdBuf := bytes.NewBufferString("")
+	// addCmd.SetOut(addCmdBuf)
 
-	s.cmd.Execute()
+	addCmdOutFile, errJsonl := toJSONL("../../etc/fixtures/complete.publication.json")
+	if errJsonl != nil {
+		t.Fatal(errJsonl)
+	}
 
-	out, err := ioutil.ReadAll(s.buf)
+	// in := strings.NewReader(addCmdOutFile)
+
+	// addCmd.SetIn(in)
+	// errAddCmd := addCmd.Execute()
+	// if errAddCmd != nil {
+	// 	t.Fatal(errAddCmd)
+	// }
+
+	// _, err := ioutil.ReadAll(addCmdBuf)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// t.Log(string(addCmdOut))
+
+	// Retrieve the publication
+	getPublication := GetPublicationCmd{}
+	getCmd := getPublication.Command()
+	getCmdBuf := bytes.NewBufferString("")
+	getCmd.SetOut(getCmdBuf)
+
+	getCmd.SetArgs([]string{"01GMQZQJZRH678JAYAW1J5VXSY"})
+	errGetCmd := getCmd.Execute()
+	if errGetCmd != nil {
+		t.Fatal(errGetCmd)
+	}
+
+	getCmdOut, err := ioutil.ReadAll(getCmdBuf)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// t.Log(string(out))
-	assert.Regexp(t, `validation failed for publication .* at line .: publication.type.required\[\/type\]`, string(out))
+	getCmdOutFile := string(getCmdOut)
+	t.Log(getCmdOutFile)
+
+	assert.JSONEqf(t, addCmdOutFile, getCmdOutFile, "error message %s", "formatted")
+
+	// assert.Regexp(t, `validation failed for publication .* at line .: publication.type.required\[\/type\]`, string(out))
 }
 
 func TestAddPublicationSuite(t *testing.T) {
 	suite.Run(t, new(AddPublicationSuite))
+}
+
+func toJSONL(f string) (string, error) {
+	file, err := os.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+	json := string(file)
+
+	json = strings.ReplaceAll(json, "  ", "")
+	json = strings.ReplaceAll(json, "\n", "")
+	json = json + "\n"
+
+	return json, nil
 }
