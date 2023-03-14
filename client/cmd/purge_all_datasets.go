6219d4f871a12265dcffca0a1a2c5919db265fc9
Matthias <matthias@colada.be>
2023-03-14 09:26:06 +0100

    Added: tests for gRPC purge-all datasets command.

--- client/cmd/purge_all_datasets.go
+++ client/cmd/purge_all_datasets.go
@@ -9,24 +9,29 @@ import (
 	"github.com/spf13/cobra"
 	api "github.com/ugent-library/biblio-backoffice/api/v1"
 	"github.com/ugent-library/biblio-backoffice/client/client"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
-	DatasetCmd.AddCommand(ImportDatasetsCmd)
-	PurgeAllDatasetsCmd.Flags().BoolP("yes", "y", false, "are you sure?")
+	DatasetCmd.AddCommand(PurgeAllDatasetsCmd)
 }
 
 var PurgeAllDatasetsCmd = &cobra.Command{
 	Use:   "purge-all",
 	Short: "Purge all datasets",
-	Run: func(cmd *cobra.Command, args []string) {
-		PurgeAllDatasets(cmd, args)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return PurgeAllDatasets(cmd, args)
 	},
 }
 
-func PurgeAllDatasets(cmd *cobra.Command, args []string) {
+func init() {
+	PurgeAllDatasetsCmd.Flags().BoolP("yes", "y", false, "are you sure?")
+}
+
+func PurgeAllDatasets(cmd *cobra.Command, args []string) error {
 	if yes, _ := cmd.Flags().GetBool("yes"); !yes {
-		return
+		cmd.Printf("no confirmation flag set. you need to set the --yes flag")
+		return nil
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
@@ -38,8 +43,26 @@ func PurgeAllDatasets(cmd *cobra.Command, args []string) {
 	if errors.Is(err, context.DeadlineExceeded) {
 		log.Fatal("ContextDeadlineExceeded: true")
 	}
-	req := &api.PurgeAllDatasetsRequest{}
-	if _, err := c.PurgeAllDatasets(context.Background(), req); err != nil {
-		log.Fatal(err)
+
+	req := &api.PurgeAllDatasetsRequest{
+		Confirm: true,
 	}
+	res, err := c.PurgeAllDatasets(context.Background(), req)
+
+	if err != nil {
+		if st, ok := status.FromError(err); ok {
+			return errors.New(st.Message())
+		}
+	}
+
+	if ge := res.GetError(); ge != nil {
+		sre := status.FromProto(ge)
+		cmd.Printf("%s", sre.Message())
+	}
+
+	if res.GetOk() {
+		cmd.Printf("purged all datasets")
+	}
+
+	return nil
 }
---
+++ client/cmd/purge_all_datasets_test.go
@@ -0,0 +1,96 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type PurgeAllDatasetsSuite struct {
+	suite.Suite
+}
+
+func (s *PurgeAllDatasetsSuite) SetupSuite() {
+	viper.Set("port", "3999")
+	viper.Set("insecure", true)
+
+	t := s.T()
+
+	file, err := os.ReadFile("../../etc/fixtures/complete.dataset.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	addCmdOutFile, err := toJSONL(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, err = addPublication(addCmdOutFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (s *PurgeAllDatasetsSuite) TestPurgeAll() {
+	t := s.T()
+
+	stdOut, _, err := purgeAllDatasets(false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "no confirmation flag set. you need to set the --yes flag", stdOut)
+
+	// Artificial sleep. PurgeAll triggers an async ES6 task. Multiple tasks registered with
+	// ES6 cause conflicts. A timeout gives ES a chance to resolve tasks sequentially.
+	time.Sleep(2 * time.Second)
+
+	stdOut, _, err = purgeAllDatasets(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "purged all datasets", stdOut)
+}
+
+func TestPurgeAllDatasetsSuite(t *testing.T) {
+	suite.Run(t, new(PurgeAllDatasetsSuite))
+}
+
+func purgeAllDatasets(confirm bool) (string, string, error) {
+	stdOut := bytes.NewBufferString("")
+	stdErr := bytes.NewBufferString("")
+
+	rootCmd.SetOut(stdOut)
+	rootCmd.SetErr(stdErr)
+
+	if confirm {
+		rootCmd.SetArgs([]string{"dataset", "purge-all", "--yes"})
+	} else {
+		rootCmd.SetArgs([]string{"dataset", "purge-all"})
+	}
+
+	errGet := rootCmd.Execute()
+	if errGet != nil {
+		return "", "", errGet
+	}
+
+	resOut, err := ioutil.ReadAll(stdOut)
+	if err != nil {
+		return "", "", err
+	}
+
+	resErr, err := ioutil.ReadAll(stdOut)
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(resOut), string(resErr), nil
+}
