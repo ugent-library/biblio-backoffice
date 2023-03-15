07019aa7156bee0ade47905bd079519cf6d28c3d
Matthias <matthias@colada.be>
2023-03-15 15:49:37 +0100

    Changed: refactored gRPC dataset reindex command, added tests.

--- client/cmd/reindex_dataset.go
+++ client/cmd/reindex_dataset.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"log"
 	"time"
@@ -11,6 +10,7 @@ import (
 	"github.com/spf13/cobra"
 	api "github.com/ugent-library/biblio-backoffice/api/v1"
 	"github.com/ugent-library/biblio-backoffice/client/client"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
@@ -20,12 +20,12 @@ func init() {
 var ReindexDatasetCmd = &cobra.Command{
 	Use:   "reindex",
 	Short: "Reindex all datasets",
-	Run: func(cmd *cobra.Command, args []string) {
-		ReindexDatasets(cmd, args)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return ReindexDatasets(cmd, args)
 	},
 }
 
-func ReindexDatasets(cmd *cobra.Command, args []string) {
+func ReindexDatasets(cmd *cobra.Command, args []string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
@@ -39,18 +39,48 @@ func ReindexDatasets(cmd *cobra.Command, args []string) {
 	req := &api.ReindexDatasetsRequest{}
 	stream, err := c.ReindexDatasets(context.Background(), req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	for {
-		res, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("error while reading stream: %v", err)
-		}
+	waitc := make(chan struct{})
+	errorc := make(chan error)
 
-		fmt.Printf("%s\n", res.Message)
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				// read done.
+				close(waitc)
+				return
+			}
+
+			// return gRPC level error
+			if err != nil {
+				errorc <- err
+				return
+			}
+
+			// Application level error
+			if ge := res.GetError(); ge != nil {
+				sre := status.FromProto(ge)
+				cmd.Printf("%s\n", sre.Message())
+			}
+
+			if rr := res.GetMessage(); rr != "" {
+				cmd.Printf("%s\n", rr)
+			}
+		}
+	}()
+
+	stream.CloseSend()
+
+	select {
+	case errc := <-errorc:
+		if st, ok := status.FromError(errc); ok {
+			return errors.New(st.Message())
+		}
+	case <-waitc:
 	}
+
+	return nil
 }
