f213767a5b7b99c4d520ad30524001cca8d8e1a0
nsteenla <nicolas.steenlant@ugent.be>
2023-06-01 17:09:43 +0200

    contributor validation

--- internal/models/contributor.go
+++ internal/models/contributor.go
@@ -1,8 +1,28 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/ugent-library/biblio-backoffice/internal/validation"
+	"github.com/ugent-library/biblio-backoffice/internal/vocabularies"
 )
+
+// type ContributorDepartment struct {
+// 	ID   string `json:"id"`
+// 	Name string `json:"name"`
+// }
+
+// TODO only name should be required (support corporate names)
+// type Contributor struct {
+// 	CreditRole []string                `json:"credit_role,omitempty"`
+// 	FirstName  string                  `json:"first_name,omitempty"`
+// 	FullName   string                  `json:"full_name,omitempty"` // TODO rename to Name
+// 	ID         string                  `json:"id,omitempty"`
+// 	LastName   string                  `json:"last_name,omitempty"`
+// 	ORCID      string                  `json:"orcid,omitempty"`
+// 	UGentID    []string                `json:"ugent_id,omitempty"`
+// 	Department []ContributorDepartment `json:"department,omitempty"`
+// }
 
 type ExternalPerson struct {
 	FullName  string `json:"name,omitempty"`
@@ -65,47 +85,34 @@ func (c *Contributor) LastName() string {
 }
 
 func (c *Contributor) Validate() (errs validation.Errors) {
-	return nil
+	if c.ExternalPerson != nil {
+		if c.ExternalPerson.FullName == "" {
+			errs = append(errs, &validation.Error{
+				Pointer: "/full_name",
+				Code:    "full_name.required",
+			})
+		}
+		if c.ExternalPerson.FirstName == "" {
+			errs = append(errs, &validation.Error{
+				Pointer: "/first_name",
+				Code:    "first_name.required",
+			})
+		}
+		if c.ExternalPerson.LastName == "" {
+			errs = append(errs, &validation.Error{
+				Pointer: "/last_name",
+				Code:    "last_name.required",
+			})
+		}
+	}
+	for i, cr := range c.CreditRole {
+		if !validation.InArray(vocabularies.Map["credit_roles"], cr) {
+			errs = append(errs, &validation.Error{
+				Pointer: fmt.Sprintf("/credit_role/%d", i),
+				Code:    "credit_role.invalid",
+			})
+		}
+	}
+
+	return
 }
-
-// type ContributorDepartment struct {
-// 	ID   string `json:"id"`
-// 	Name string `json:"name"`
-// }
-
-// TODO only name should be required (support corporate names)
-// type Contributor struct {
-// 	CreditRole []string                `json:"credit_role,omitempty"`
-// 	FirstName  string                  `json:"first_name,omitempty"`
-// 	FullName   string                  `json:"full_name,omitempty"` // TODO rename to Name
-// 	ID         string                  `json:"id,omitempty"`
-// 	LastName   string                  `json:"last_name,omitempty"`
-// 	ORCID      string                  `json:"orcid,omitempty"`
-// 	UGentID    []string                `json:"ugent_id,omitempty"`
-// 	Department []ContributorDepartment `json:"department,omitempty"`
-// }
-
-// func (c *Contributor) Validate() (errs validation.Errors) {
-// 	if c.FirstName == "" {
-// 		errs = append(errs, &validation.Error{
-// 			Pointer: "/first_name",
-// 			Code:    "first_name.required",
-// 		})
-// 	}
-// 	if c.LastName == "" {
-// 		errs = append(errs, &validation.Error{
-// 			Pointer: "/last_name",
-// 			Code:    "last_name.required",
-// 		})
-// 	}
-// 	for i, cr := range c.CreditRole {
-// 		if !validation.InArray(vocabularies.Map["credit_roles"], cr) {
-// 			errs = append(errs, &validation.Error{
-// 				Pointer: fmt.Sprintf("/credit_role/%d", i),
-// 				Code:    "credit_role.invalid",
-// 			})
-// 		}
-// 	}
-
-// 	return
-// }
