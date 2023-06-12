924abbe50166ee8d9837769acdbce9b3ef072654
Koen Verheyen <verheyen.koen@gmail.com>
2023-06-12 10:22:15 +0200

    Cypress test for issue #1123

---
+++ cypress/e2e/issues/issue-1123.cy.ts
@@ -0,0 +1,24 @@
+describe('Issue #1123:  WoS import cuts keywords up because of newlines in import', () => {
+  it('should not split up keywords by newlines', () => {
+    cy.loginAsLibrarian()
+
+    cy.visit('/publication/add')
+
+    cy.contains('Import from Web of Science').click()
+    cy.contains('.btn', 'Add publication(s)').click()
+
+    cy.get('input[name=file]').selectFile('cypress/fixtures/wos-000963572100001.txt')
+
+    cy.contains('a.list-group-item', 'Description').click()
+
+    cy.contains('label', 'Keywords')
+      .next('div')
+      .find('ul > li > span')
+      .then($spans => Cypress._.map($spans, 'innerText'))
+      .should('include.members', ['testosterone treatment', 'in vitro maturation'])
+      .should('not.include', 'testosterone')
+      .should('not.include', 'treatment')
+      .should('not.include', 'in vitro')
+      .should('not.include', 'maturation')
+  })
+})
