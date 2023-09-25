9ba427ddde5d2d6384c62e84962714a3f9c0c4de
nsteenla <nicolas.steenlant@ugent.be>
2023-09-25 15:52:28 +0200

    fix custom popover js

--- assets/js/ui/popover.js
+++ assets/js/ui/popover.js
@@ -3,7 +3,7 @@ import {Popover} from "bootstrap.native";
 
 export default function () {
     let addEvents = function(rootEl) {
-        rootEl.querySelectorAll('[data-toggle=popover-custom]').forEach(function(el) {
+        rootEl.querySelectorAll('[data-bs-toggle=popover-custom]').forEach(function(el) {
             let container = document.querySelector(el.dataset.popoverContent)
             let content = container.querySelector('.popover-body')
             let heading = container.querySelector('.popover-heading')
