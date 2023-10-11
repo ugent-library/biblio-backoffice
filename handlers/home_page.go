84cbb7039286224777e56b61f0617c2f10336744
nsteenla <nicolas.steenlant@ugent.be>
2023-10-11 12:21:03 +0200

    give each handler own file

--- handlers/home_page.go
+++ handlers/home_page.go
@@ -4,57 +4,10 @@ import (
 	"net/http"
 
 	"github.com/ugent-library/biblio-backoffice/ctx"
-	"github.com/ugent-library/biblio-backoffice/models"
 	"github.com/ugent-library/biblio-backoffice/views"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	c := ctx.Get(r)
 	views.HomePage(c).Render(r.Context(), w)
-}
-
-func ActionRequired(w http.ResponseWriter, r *http.Request) {
-	c := ctx.Get(r)
-
-	pHits, err := c.PublicationSearchIndex.Search(models.NewSearchArgs().
-		WithPageSize(0).
-		WithFilter("creator_id|author_id", c.User.ID).
-		WithFilter("status", "returned"))
-	if err != nil {
-		c.HandleError(w, r, err)
-		return
-	}
-	dHits, err := c.DatasetSearchIndex.Search(models.NewSearchArgs().
-		WithPageSize(0).
-		WithFilter("creator_id|author_id", c.User.ID).
-		WithFilter("status", "returned"))
-	if err != nil {
-		c.HandleError(w, r, err)
-		return
-	}
-
-	views.ActionRequired(c, pHits.Total, dHits.Total).Render(r.Context(), w)
-}
-
-func DraftsToComplete(w http.ResponseWriter, r *http.Request) {
-	c := ctx.Get(r)
-
-	pHits, err := c.PublicationSearchIndex.Search(models.NewSearchArgs().
-		WithPageSize(0).
-		WithFilter("creator_id|author_id", c.User.ID).
-		WithFilter("status", "private"))
-	if err != nil {
-		c.HandleError(w, r, err)
-		return
-	}
-	dHits, err := c.DatasetSearchIndex.Search(models.NewSearchArgs().
-		WithPageSize(0).
-		WithFilter("creator_id|author_id", c.User.ID).
-		WithFilter("status", "private"))
-	if err != nil {
-		c.HandleError(w, r, err)
-		return
-	}
-
-	views.DraftsToComplete(c, pHits.Total, dHits.Total).Render(r.Context(), w)
 }
