7922e7f8bd97db214481394db11726deb23f8c3e
Koen Verheyen <verheyen.koen@gmail.com>
2023-10-16 15:06:24 +0200

    Cypress: improved ensureModal command (return #modal element instead of #modals container because Cypress thinks that is invisible)

--- cypress/support/commands/ensure-modal.ts
+++ cypress/support/commands/ensure-modal.ts
@@ -3,26 +3,22 @@ import { logCommand } from './helpers'
 export default function ensureModal(expectedTitle: string | RegExp): Cypress.Chainable<JQuery<HTMLElement>> {
   const log = logCommand('ensureModal', { 'Expected title': expectedTitle }, expectedTitle.toString())
 
-  return cy
-    .get('#modals', { log: false })
-    .within({ log: false }, () => {
-      // Assertion "be.visible" doesn't work here because it is behind the dialog
-      cy.get('#modal-backdrop', { log: false }).then(modalBackdrop => {
-        if (!modalBackdrop.get(0).classList.contains('show')) {
-          cy.wrap(modalBackdrop, { log: false }).should('have.class', 'show')
-        }
-      })
+  // Assertion "be.visible" doesn't work here because it is behind the dialog
+  cy.get('#modal-backdrop', { log: false }).then(modalBackdrop => {
+    if (!modalBackdrop.get(0).classList.contains('show')) {
+      cy.wrap(modalBackdrop, { log: false }).should('have.class', 'show')
+    }
+  })
 
-      return cy
-        .get('#modal', { log: false })
-        .should('be.visible')
-        .within({ log: false }, () => {
-          if (expectedTitle instanceof RegExp) {
-            cy.get('.modal-title', { log: false }).invoke({ log: false }, 'text').should('match', expectedTitle)
-          } else {
-            cy.get('.modal-title', { log: false }).should('have.text', expectedTitle)
-          }
-        })
+  return cy
+    .get('#modal', { log: false })
+    .should('be.visible')
+    .within({ log: false }, () => {
+      if (expectedTitle instanceof RegExp) {
+        cy.get('.modal-title', { log: false }).invoke({ log: false }, 'text').should('match', expectedTitle)
+      } else {
+        cy.get('.modal-title', { log: false }).should('have.text', expectedTitle)
+      }
     })
     .finishLog(log)
 }
