3cf14998d82114a06b3e973c9ce5235e46ecaba1
nsteenla <nicolas.steenlant@ugent.be>
2023-10-26 15:06:00 +0200

    remove dependency on ugent-library/middleware

--- cli/server_cmd.go
+++ cli/server_cmd.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/sessions"
 	"github.com/nics/ich"
-	"github.com/oklog/ulid/v2"
 	"github.com/ory/graceful"
 	"github.com/spf13/cobra"
 	"github.com/ugent-library/biblio-backoffice/backends"
@@ -23,11 +22,8 @@ import (
 	"github.com/ugent-library/biblio-backoffice/routes"
 	"github.com/ugent-library/biblio-backoffice/urls"
 	"github.com/ugent-library/biblio-backoffice/vocabularies"
-	"github.com/ugent-library/middleware"
 	"github.com/ugent-library/mix"
 	"github.com/ugent-library/oidc"
-	"github.com/ugent-library/zaphttp"
-	"go.uber.org/zap"
 
 	_ "github.com/ugent-library/biblio-backoffice/translations"
 )
@@ -57,26 +53,11 @@ var serverStartCmd = &cobra.Command{
 			return err
 		}
 
-		// apply these before request reaches the router
-		handler := middleware.Apply(router,
-			middleware.Recover(func(err any) {
-				if config.Env == "local" {
-					logger.With(zap.Stack("stack")).Error(err)
-				} else {
-					logger.Error(err)
-				}
-			}),
-			middleware.SetRequestID(func() string {
-				return ulid.Make().String()
-			}),
-			zaphttp.LogRequests(logger.Desugar()),
-		)
-
 		// setup server
 		addr := fmt.Sprintf("%s:%d", config.Host, config.Port)
 		server := graceful.WithDefaults(&http.Server{
 			Addr:         addr,
-			Handler:      handler,
+			Handler:      router,
 			ReadTimeout:  5 * time.Minute,
 			WriteTimeout: 5 * time.Minute,
 		})
