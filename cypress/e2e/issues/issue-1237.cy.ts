7e9e2a51d5fd804f3987b2d8cc86ec53bfc70367
Koen Verheyen <verheyen.koen@gmail.com>
2023-11-21 10:55:33 +0100

    Cypress: test stability improvements for #1237

--- cypress/e2e/issues/issue-1237.cy.ts
+++ cypress/e2e/issues/issue-1237.cy.ts
@@ -508,22 +508,24 @@ describe('Issue #1237: Accessibility and mark-up: make sure labels are clickable
 
   function testFocusForLabel(labelText: string, fieldSelector: string, autoFocus = false) {
     cy.getLabel(labelText)
-      .as('theLabel')
+      .as('theLabel', { type: 'static' })
       .should('have.length', 1)
       .parent({ log: false })
       .find(fieldSelector)
-      .first()
-      .as('theField')
       .should('have.length', 1)
-      .should(autoFocus ? 'have.focus' : 'not.have.focus')
+      .first({ log: false })
+      .as('theField', { type: 'static' })
+      .should(autoFocus ? 'be.focused' : 'not.be.focused')
 
     if (autoFocus) {
       cy.focused().blur()
+
+      cy.get('@theField').should('not.be.focused')
     }
 
     cy.get('@theLabel').click()
 
-    cy.get('@theField').should('have.focus')
+    cy.get('@theField').should('be.focused')
   }
 
   function testConferenceDetailsSection() {
