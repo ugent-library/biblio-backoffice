da0cd25fa562e46899374c3017ec2f4455f67dd7
Koen Verheyen <verheyen.koen@gmail.com>
2023-12-14 09:21:25 +0100

    Cypress: improved ensureToast command

--- cypress/support/commands/ensure-toast.ts
+++ cypress/support/commands/ensure-toast.ts
@@ -1,10 +1,25 @@
 import { logCommand } from './helpers'
 
-export default function ensureToast(expectedTitle: string | RegExp): Cypress.Chainable<JQuery<HTMLElement>> {
+export default function ensureToast(expectedTitle?: string | RegExp): Cypress.Chainable<JQuery<HTMLElement>> {
   const log = logCommand('ensureToast', { 'Expected title': expectedTitle }, expectedTitle)
 
   return cy
-    .contains('.toast', expectedTitle, { log: false })
+    .get('.toast', { log: false })
+    .should(toast => {
+      if (toast.length !== 1) {
+        expect(toast).to.have.length(1)
+      }
+
+      if (expectedTitle) {
+        if (typeof expectedTitle === 'string') {
+          expect(toast).to.contain(expectedTitle)
+        } else {
+          expect(toast).to.match(expectedTitle)
+        }
+      }
+
+      return toast
+    })
     .should('be.visible')
 
     .within({ log: false }, () => {})
@@ -14,7 +29,7 @@ export default function ensureToast(expectedTitle: string | RegExp): Cypress.Cha
 declare global {
   namespace Cypress {
     interface Chainable {
-      ensureToast(expectedTitle: string | RegExp): Chainable<JQuery<HTMLElement>>
+      ensureToast(expectedTitle?: string | RegExp): Chainable<JQuery<HTMLElement>>
     }
   }
 }
