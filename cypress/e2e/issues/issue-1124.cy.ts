c26c977fb291c31c5d5391ca6db609effaa5f541
Koen Verheyen <verheyen.koen@gmail.com>
2024-01-19 09:48:05 +0100

    Improved issue #1124 tests

--- cypress/e2e/issues/issue-1124.cy.ts
+++ cypress/e2e/issues/issue-1124.cy.ts
@@ -2,103 +2,94 @@ describe('Issue #1124:  Add friendlier consistent confirmation toaster when lock
   it('should not split up keywords by newlines', () => {
     cy.loginAsResearcher()
 
-    cy.visit('/publication/add')
+    cy.setUpPublication('Dissertation')
 
-    cy.contains('Import from Web of Science').click()
-    cy.contains('.btn', 'Add publication(s)').click()
+    cy.get<string>('@biblioId').then(biblioId => {
+      // Publication does not have lock icon in the list view
+      cy.visit('/publication')
+      cy.get('input[name=q][placeholder="Search..."]').type(biblioId)
+      cy.contains('.btn', 'Search').click()
+      cy.contains('Publications Showing 1').should('be.visible')
+      cy.get('.list-group-item .if.if-lock').should('not.exist')
+      cy.contains('.list-group-item', 'Locked').should('not.exist')
 
-    cy.get('input[name=file]').selectFile('cypress/fixtures/wos-000863180800005.txt')
+      // Publication does not have lock icon in the detail view
+      cy.visitPublication()
+      cy.get('.c-subline .if.if-lock').should('not.exist')
+      cy.contains('.c-subline', 'Locked').should('not.exist')
 
-    cy.contains('Biblio ID:')
-      .find('code')
-      .invoke('text')
-      .as('biblioId')
-      .then(biblioId => {
-        // Publication does not have lock icon in the list view
-        cy.visit('/publication')
-        cy.get('input[name=q][placeholder="Search..."]').type(biblioId)
-        cy.contains('.btn', 'Search').click()
-        cy.contains('Publications Showing 1').should('be.visible')
-        cy.get('.list-group-item .if.if-lock').should('not.exist')
-        cy.contains('.list-group-item', 'Locked').should('not.exist')
+      // Edit button is still available for researchers for unlocked publications
+      cy.contains('.btn', 'Edit').should('be.visible').click()
+      cy.ensureModal('Edit publication details')
 
-        // Publication does not have lock icon in the detail view
-        cy.visit(`/publication/${biblioId}`)
-        cy.get('.c-subline .if.if-lock').should('not.exist')
-        cy.contains('.c-subline', 'Locked').should('not.exist')
+      // Publication cannot be locked by researcher
+      cy.contains('.btn', 'Lock record').should('not.exist')
 
-        // Edit button is still available for researchers for unlocked publications
-        cy.contains('.btn', 'Edit').should('be.visible').click()
-        cy.ensureModal('Edit publication details')
+      // Publication can be locked by librarian
+      cy.loginAsLibrarian()
+      cy.switchMode('Librarian')
 
-        // Publication cannot be locked by researcher
-        cy.contains('.btn', 'Lock record').should('not.exist')
+      cy.visit('/publication')
+      cy.get('input[name=q][placeholder="Search..."]').type(biblioId)
+      cy.contains('.btn', 'Search').click()
+      cy.contains('Publications Showing 1').should('be.visible')
+      cy.get('.list-group-item .if.if-lock').should('not.exist')
+      cy.contains('.list-group-item', 'Locked').should('not.exist')
 
-        // Publication can be locked by librarian
-        cy.loginAsLibrarian()
-        cy.switchMode('Librarian')
+      cy.visitPublication()
+      cy.contains('.btn', 'Lock record').should('be.visible').click()
 
-        cy.visit('/publication')
-        cy.get('input[name=q][placeholder="Search..."]').type(biblioId)
-        cy.contains('.btn', 'Search').click()
-        cy.contains('Publications Showing 1').should('be.visible')
-        cy.get('.list-group-item .if.if-lock').should('not.exist')
-        cy.contains('.list-group-item', 'Locked').should('not.exist')
+      // Confirmation toast is displayed upon locking (and hidden after 5s)
+      cy.contains('.toast', 'Publication was successfully locked.').as('lockedToast').should('be.visible')
+      cy.wait(5000)
+      cy.get('@lockedToast').should('not.exist')
 
-        cy.visit(`/publication/${biblioId}`)
-        cy.contains('.btn', 'Lock record').should('be.visible').click()
+      // Publication now has lock icon in the detail view
+      cy.get('.c-subline .if.if-lock').should('be.visible')
+      cy.contains('.c-subline', 'Locked').should('be.visible')
 
-        // Confirmation toast is displayed upon locking (and hidden after 5s)
-        cy.contains('.toast', 'Publication was successfully locked.').as('lockedToast').should('be.visible')
-        cy.wait(5000)
-        cy.get('@lockedToast').should('not.exist')
+      // Edit button is still available for librarians for locked publications
+      cy.contains('.btn', 'Edit').should('be.visible').click()
+      cy.ensureModal('Edit publication details')
 
-        // Publication now has lock icon in the detail view
-        cy.get('.c-subline .if.if-lock').should('be.visible')
-        cy.contains('.c-subline', 'Locked').should('be.visible')
+      // Publication does have lock icon in the list view for librarians
+      cy.visit('/publication', { qs: { q: biblioId } })
+      cy.contains('Publications Showing 1').should('be.visible')
+      cy.get('.list-group-item .if.if-lock').should('be.visible')
+      cy.contains('.list-group-item', 'Locked').should('be.visible')
 
-        // Edit button is still available for librarians for locked publications
-        cy.contains('.btn', 'Edit').should('be.visible').click()
-        cy.ensureModal('Edit publication details')
+      // Publication does have lock icon in the list view for researcher
+      cy.loginAsResearcher()
+      cy.visit('/publication', { qs: { q: biblioId, 'f[scope]': 'all' } })
+      cy.contains('Publications Showing 1').should('be.visible')
+      cy.get('.list-group-item .if.if-lock').should('be.visible')
+      cy.contains('.list-group-item', 'Locked').should('be.visible')
 
-        // Publication does have lock icon in the list view for librarians
-        cy.visit('/publication', { qs: { q: biblioId } })
-        cy.contains('Publications Showing 1').should('be.visible')
-        cy.get('.list-group-item .if.if-lock').should('be.visible')
-        cy.contains('.list-group-item', 'Locked').should('be.visible')
+      // In the detail view, as a researcher...
+      cy.visitPublication()
 
-        // Publication does have lock icon in the list view for researcher
-        cy.loginAsResearcher()
-        cy.visit('/publication', { qs: { q: biblioId, 'f[scope]': 'all' } })
-        cy.contains('Publications Showing 1').should('be.visible')
-        cy.get('.list-group-item .if.if-lock').should('be.visible')
-        cy.contains('.list-group-item', 'Locked').should('be.visible')
+      // Publication now has a lock icon
+      cy.get('.c-subline .if.if-lock').should('be.visible')
+      cy.contains('.c-subline', 'Locked').should('be.visible')
 
-        // In the detail view, as a researcher...
-        cy.visit(`/publication/${biblioId}`)
+      // Edit button is no longer available
+      cy.contains('.btn', 'Edit').should('not.exist')
 
-        // Publication now has a lock icon
-        cy.get('.c-subline .if.if-lock').should('be.visible')
-        cy.contains('.c-subline', 'Locked').should('be.visible')
+      // Now go back as librarian to unlock the publication
+      cy.loginAsLibrarian()
+      cy.switchMode('Librarian')
 
-        // Edit button is no longer available
-        cy.contains('.btn', 'Edit').should('not.exist')
+      cy.visitPublication()
+      cy.contains('.btn', 'Unlock record').should('be.visible').click()
 
-        // Now go back as librarian to unlock the publication
-        cy.loginAsLibrarian()
-        cy.switchMode('Librarian')
+      // Confirmation toast is displayed upon locking (and hidden after 5s)
+      cy.contains('.toast', 'Publication was successfully unlocked.').as('unlockedToast').should('be.visible')
+      cy.wait(5000)
+      cy.get('@unlockedToast').should('not.exist')
 
-        cy.visit(`/publication/${biblioId}`)
-        cy.contains('.btn', 'Unlock record').should('be.visible').click()
-
-        // Confirmation toast is displayed upon locking (and hidden after 5s)
-        cy.contains('.toast', 'Publication was successfully unlocked.').as('unlockedToast').should('be.visible')
-        cy.wait(5000)
-        cy.get('@unlockedToast').should('not.exist')
-
-        // Lock icon is removed again
-        cy.get('.c-subline .if.if-lock').should('not.exist')
-        cy.contains('.c-subline', 'Locked').should('not.exist')
-      })
+      // Lock icon is removed again
+      cy.get('.c-subline .if.if-lock').should('not.exist')
+      cy.contains('.c-subline', 'Locked').should('not.exist')
+    })
   })
 })
