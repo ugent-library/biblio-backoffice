5cbf9f0e1db85d20add03f1be54295d0e04761bb
Koen Verheyen <verheyen.koen@gmail.com>
2024-03-18 12:03:39 +0100

    Cypress: suppress /dashboard-icon route from flushing command log

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,3 +4,8 @@ import "./commands";
 import "./queries";
 
 Cypress.Keyboard.defaults({ keystrokeDelay: 0 });
+
+beforeEach(() => {
+  // Keep dashboard-icon polling from Cypress command log
+  cy.intercept({ method: "GET", url: "/dashboard-icon" }, { log: false });
+});
