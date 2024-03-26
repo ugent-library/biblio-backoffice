9cd825bed54709422764df148a68fac37b0fafd3
nsteenla <nicolas.steenlant@ugent.be>
2024-03-26 14:17:59 +0100

    fix importing organizations

--- db/migrations/015_add_publication_count.sql
+++ db/migrations/015_add_publication_count.sql
@@ -3,5 +3,5 @@ ALTER TABLE projects ADD COLUMN publication_count INT NOT NULL DEFAULT 0;
 
 ---- create above / drop below ----
 
-ALTER TABLE people DROP COLUMN publication_count INT;
-ALTER TABLE projects DROP COLUMN publication_count INT;
+ALTER TABLE people DROP COLUMN publication_count;
+ALTER TABLE projects DROP COLUMN publication_count;
