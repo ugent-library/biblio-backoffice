e8d4e09029892c7349d4a1030158590323c7131e
Koen Verheyen <verheyen.koen@gmail.com>
2024-03-29 18:17:44 +0100

    Added views.toJSON helper #1402

--- views/views.go
+++ views/views.go
@@ -1,3 +1,17 @@
+//go:generate go run github.com/a-h/templ/cmd/templ@v0.2.543 generate
 package views
 
-//go:generate go run github.com/a-h/templ/cmd/templ@v0.2.543 generate
+import (
+	"encoding/json"
+)
+
+func ToJSON(pairs ...string) string {
+	data := make(map[string]string)
+
+	for i := 0; i < len(pairs); i += 2 {
+		data[pairs[i]] = pairs[i+1]
+	}
+
+	bytes, _ := json.Marshal(data)
+	return string(bytes)
+}
