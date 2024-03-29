0fc6a9fe18defa8cf5455d9739c12ddf9f84279a
Koen Verheyen <verheyen.koen@gmail.com>
2024-03-29 18:14:32 +0100

    Made ShowModalLayout view public #1402

--- views/confirm_reject_candidate_record_templ.go
+++ views/confirm_reject_candidate_record_templ.go
@@ -51,7 +51,7 @@ func ConfirmRejectCandidateRecord(c *ctx.Ctx, rec *models.CandidateRecord) templ
 			}
 			return templ_7745c5c3_Err
 		})
-		templ_7745c5c3_Err = showModalLayout(c, nil).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = ShowModalLayout(c, nil).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
