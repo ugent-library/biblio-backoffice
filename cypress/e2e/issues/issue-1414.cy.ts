27a603f906cec5c5d6e4172042372b503f772fd5
Koen Verheyen <verheyen.koen@gmail.com>
2024-04-19 11:38:23 +0200

    Added failing test for #1414

---
+++ cypress/e2e/issues/issue-1414.cy.ts
@@ -0,0 +1,19 @@
+// https://github.com/ugent-library/biblio-backoffice/issues/1414
+
+describe("Issue #1414: JS error when closing toast", () => {
+  it("should not error when you close a toast manually before auto dismissal", () => {
+    cy.loginAsLibrarian();
+
+    cy.setUpPublication();
+    cy.visitPublication();
+
+    cy.contains(".btn", "Lock record").click();
+
+    cy.wait(3000);
+
+    cy.ensureToast("Publication was successfully locked.").closeToast();
+
+    // The error occurred after 5000ms, so we wait another 3000ms to make sure the test hasn't succeeded by that time
+    cy.wait(3000);
+  });
+});
