21169892326de371ae08ea7c79bb6d1f91a69e5c
netsensei <matthias@colada.be>
2024-04-25 19:10:07 +0000

    Fixed: skip duplicate candidate records check did not work.

--- cli/update_candidate_records_cmd.go
+++ cli/update_candidate_records_cmd.go
@@ -2,9 +2,11 @@ package cli
 
 import (
 	"context"
+	"errors"
 
 	"github.com/spf13/cobra"
 
+	"github.com/ugent-library/biblio-backoffice/models"
 	"github.com/ugent-library/biblio-backoffice/recordsources"
 	"github.com/ugent-library/biblio-backoffice/recordsources/plato"
 )
@@ -30,12 +32,15 @@ var updateCandidateRecords = &cobra.Command{
 
 		err = src.GetRecords(context.Background(), func(srcRec recordsources.Record) error {
 			oldCandidateRec, err := services.Repo.GetCandidateRecordBySource(context.TODO(), srcRec.SourceName(), srcRec.SourceID())
+			if err != nil {
+				if !errors.Is(err, models.ErrNotFound) {
+					return err
+				}
+			}
+
 			if oldCandidateRec != nil {
 				zapLogger.Warnf("skipping duplicate candidate record from source %s/%s: already found in %s", srcRec.SourceName(), srcRec.SourceID(), oldCandidateRec.ID)
 				return nil
-			}
-			if err != nil {
-				return err
 			}
 
 			candidateRec, err := srcRec.ToCandidateRecord(services)
