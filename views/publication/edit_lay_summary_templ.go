b6b02cd88a5e5b82e5d04c469f1e675c079b96c4
nsteenla <nicolas.steenlant@ugent.be>
2024-05-14 13:49:44 +0000

    boilerplate

--- views/publication/edit_lay_summary_templ.go
+++ views/publication/edit_lay_summary_templ.go
@@ -19,7 +19,7 @@ import (
 	"github.com/ugent-library/okay"
 )
 
-func EditAbstractDialog(c *ctx.Ctx, p *models.Publication, abstract *models.Text, idx int, conflict bool, errors *okay.Errors, isNew bool) templ.Component {
+func EditLaySummaryDialog(c *ctx.Ctx, p *models.Publication, abstract *models.Text, idx int, conflict bool, errors *okay.Errors, isNew bool) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
