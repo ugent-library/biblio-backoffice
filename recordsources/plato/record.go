d1338cbfa4c79127b5576e6df21117fc1252c4e8
netsensei <matthias@colada.be>
2024-06-03 07:40:32 +0000

    Changed: set default language to dutch on abstracts for plato records.

--- recordsources/plato/record.go
+++ recordsources/plato/record.go
@@ -109,7 +109,7 @@ func (r *platoRecord) ToCandidateRecord(services *backends.Services) (*models.Ca
 		p.ISBN = append(p.ISBN, v.String())
 	}
 	if v := md.Get("pdf.abstract"); v.Exists() {
-		p.AddAbstract(&models.Text{Lang: "und", Text: v.String()})
+		p.AddAbstract(&models.Text{Lang: "dut", Text: v.String()})
 	}
 	if v := md.Get("pdf.url"); v.Exists() {
 		sha256, size, err := recordsources.StoreURL(context.TODO(), v.String(), services.FileStore)
