bedf9118da5f5b37c58e6add173bc9104e266e05
nsteenla <nicolas.steenlant@ugent.be>
2024-06-04 15:14:56 +0200

    add candidate records seed

--- cli/seed_cmd.go
+++ cli/seed_cmd.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/ugent-library/biblio-backoffice/models"
 	"github.com/ugent-library/biblio-backoffice/people"
 	"github.com/ugent-library/biblio-backoffice/projects"
 )
@@ -23,6 +24,9 @@ func init() {
 
 	seedProjectsCmd.Flags().Bool("force", false, "force seeding the database")
 	seedCmd.AddCommand(seedProjectsCmd)
+
+	seedCandidateRecordsCmd.Flags().Bool("force", false, "force seeding the database")
+	seedCmd.AddCommand(seedCandidateRecordsCmd)
 }
 
 var seedCmd = &cobra.Command{
@@ -32,7 +36,7 @@ var seedCmd = &cobra.Command{
 
 var seedOrganizationsCmd = &cobra.Command{
 	Use:   "organizations",
-	Short: "seed organizations data",
+	Short: "seed organization data",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		peopleRepo := newServices().PeopleRepo
 
@@ -74,7 +78,7 @@ var seedOrganizationsCmd = &cobra.Command{
 
 var seedPeopleCmd = &cobra.Command{
 	Use:   "people",
-	Short: "seed people data",
+	Short: "seed person data",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		peopleRepo := newServices().PeopleRepo
 
@@ -98,7 +102,7 @@ var seedPeopleCmd = &cobra.Command{
 				return err
 			}
 			if err := peopleRepo.ImportPerson(context.TODO(), params); err != nil {
-				continue
+				return err
 			}
 			logger.Info("imported person", "username", params.Username)
 		}
@@ -109,7 +113,7 @@ var seedPeopleCmd = &cobra.Command{
 
 var seedProjectsCmd = &cobra.Command{
 	Use:   "projects",
-	Short: "seed projects' data",
+	Short: "seed project data",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		projectsRepo := newServices().ProjectsRepo
 
@@ -133,11 +137,47 @@ var seedProjectsCmd = &cobra.Command{
 				return err
 			}
 			if err := projectsRepo.ImportProject(context.TODO(), params); err != nil {
-				continue
+				return err
 			}
 			logger.Info("imported project", "iwetoID", params.Identifiers.Get("iweto"))
 		}
 
 		return newServices().ProjectsIndex.ReindexProjects(context.TODO(), projectsRepo.EachProject)
+	},
+}
+
+var seedCandidateRecordsCmd = &cobra.Command{
+	Use:   "candidate-records",
+	Short: "seed candidate record data",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo := newServices().Repo
+
+		if force, _ := cmd.Flags().GetBool("force"); !force {
+			count, err := repo.CountCandidateRecords(cmd.Context())
+			if err != nil {
+				return err
+			}
+			if count > 0 {
+				logger.Warn("not seeding dummy data because the database is not empty")
+				return nil
+			}
+		}
+
+		dec := json.NewDecoder(os.Stdin)
+		for {
+			rec := &models.CandidateRecord{}
+			if err := dec.Decode(rec); errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				return err
+			}
+			logger.Info("imported candidate record", "rec", rec)
+			if err := repo.AddCandidateRecord(context.TODO(), rec); err != nil {
+				return err
+			}
+			logger.Info("imported candidate record", "sourceID", rec.SourceID, "source", rec.SourceName)
+		}
+
+		return nil
 	},
 }
--- models/candidate_record.go
+++ models/candidate_record.go
@@ -6,14 +6,14 @@ import (
 )
 
 type CandidateRecord struct {
-	ID             string
-	SourceName     string
-	SourceID       string
-	SourceMetadata []byte
-	Type           string
-	Metadata       json.RawMessage
-	Status         string
-	DateCreated    time.Time
+	ID             string          `json:"id"`
+	SourceName     string          `json:"source_name"`
+	SourceID       string          `json:"source_id"`
+	SourceMetadata []byte          `json:"source_metadata"`
+	Type           string          `json:"type"`
+	Metadata       json.RawMessage `json:"metadata"`
+	Status         string          `json:"status"`
+	DateCreated    time.Time       `json:"date_created"`
 	// TODO tightly coupled with Publication for now, refactor later
-	Publication *Publication
+	Publication *Publication `json:"publication"`
 }
