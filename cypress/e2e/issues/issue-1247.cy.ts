960f6f84beafbe05af86a7bd8858aacf0ea8919f
Koen Verheyen <verheyen.koen@gmail.com>
2024-07-17 16:51:19 +0200

    Redesign of manual publication type selection page (with buttons instead of radio's) #1660

--- cypress/e2e/issues/issue-1247.cy.ts
+++ cypress/e2e/issues/issue-1247.cy.ts
@@ -38,8 +38,7 @@ describe("Issue #1247: User menu popup hidden behind publication details", () =>
 
     assertUserMenuWorks();
 
-    cy.contains("Miscellaneous").click();
-    cy.contains(".btn", "Add publication(s)").click();
+    cy.contains(".card", "Miscellaneous").contains(".btn", "Add").click();
 
     cy.location("pathname").should(
       "eq",
