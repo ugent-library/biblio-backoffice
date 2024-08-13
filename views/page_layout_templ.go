9549df2d19bcbe5f89be187d1823d0d9ee7e31ad
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2024-08-13 16:59:37 +0200

    mark proxies as beta

--- views/page_layout_templ.go
+++ views/page_layout_templ.go
@@ -597,7 +597,7 @@ func PageLayout(c *ctx.Ctx, args PageLayoutArgs) templ.Component {
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><span class=\"c-sidebar__icon\"><i class=\"if if-user-add\"></i></span> <span class=\"c-sidebar__label\">Proxies</span></a></li>")
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><span class=\"c-sidebar__icon\"><i class=\"if if-user-add\"></i></span> <span class=\"c-sidebar__label\">Proxies [beta]</span></a></li>")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
