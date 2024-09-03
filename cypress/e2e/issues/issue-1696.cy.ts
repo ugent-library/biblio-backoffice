c94bf216942f44e6db96f997eb2dda736433f50a
Koen Verheyen <verheyen.koen@gmail.com>
2024-09-03 11:56:29 +0200

    Added similar test for datasets

--- cypress/e2e/issues/issue-1696.cy.ts
+++ cypress/e2e/issues/issue-1696.cy.ts
@@ -1,11 +1,30 @@
 // https://github.com/ugent-library/biblio-backoffice/issues/1696
 
 describe("Issue #1696: <p> and </p> tags showing in project description", () => {
-  it("should strip HTML from project descriptions", () => {
+  it("should strip HTML from project descriptions in publications", () => {
     cy.loginAsResearcher("researcher1");
 
     cy.setUpPublication();
     cy.visitPublication();
+
+    cy.contains(".card", "Project").contains(".btn", "Add project").click();
+
+    cy.ensureModal("Select projects").within(() => {
+      cy.setFieldByLabel("Search project", "001D07903");
+
+      cy.get("#project-suggestions .list-group-item")
+        .should("have.length", 1)
+        .find(".list-group-item-main .text-muted")
+        .should("not.contain", "<")
+        .and("not.contain", ">");
+    });
+  });
+
+  it("should strip HTML from project descriptions in datasets", () => {
+    cy.loginAsResearcher("researcher1");
+
+    cy.setUpDataset();
+    cy.visitDataset();
 
     cy.contains(".card", "Project").contains(".btn", "Add project").click();
 
