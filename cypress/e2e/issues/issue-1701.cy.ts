009263917f56bb1d97c6aff4282117085c3177e7
Koen Verheyen <verheyen.koen@gmail.com>
2024-09-10 17:03:06 +0200

    Improved tests #1701

--- cypress/e2e/issues/issue-1701.cy.ts
+++ cypress/e2e/issues/issue-1701.cy.ts
@@ -10,7 +10,7 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
   });
 
   describe("Publication authors", () => {
-    it("should not remove two authors when two users simultaneously remove the same author", () => {
+    beforeEach(() => {
       cy.setUpPublication();
       cy.addAuthor("Author", "1", { external: true });
       cy.addAuthor("Author", "2", { external: true });
@@ -22,7 +22,52 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
         "Author 1",
         "Author 2",
       ]);
+    });
 
+    it("should remove the correct author when reordered by another user", () => {
+      // Perform the reorder via AJAX
+      cy.get("#contributors-author-table tbody")
+        .invoke("attr", "hx-headers")
+        .then(JSON.parse)
+        .then((htmxValues) => {
+          cy.get<string>("@biblioId").then((biblioId) => {
+            // Reverse the authors
+            const form = new URLSearchParams();
+            form.append("position", "1");
+            form.append("position", "0");
+
+            cy.htmxRequest({
+              method: "POST",
+              url: `/publication/${biblioId}/contributors/author/order`,
+              headers: htmxValues,
+              form: true,
+              body: form.toString(),
+              log: true,
+            });
+          });
+        });
+
+      // Page is not refreshed so we still see the original order
+      verifyContributors("#contributors-author-table", [
+        "Author 1",
+        "Author 2",
+      ]);
+
+      // Now let's try to delete Author 1
+      cy.contains("tr", "Author 1").find(".btn .if-delete").click();
+      cy.ensureModal("Confirm deletion").closeModal("Delete");
+
+      cy.verifyConflictErrorDialog("publication").closeModal("Close");
+
+      cy.reload();
+
+      verifyContributors("#contributors-author-table", [
+        "Author 2",
+        "Author 1",
+      ]);
+    });
+
+    it("should not remove two authors when two users simultaneously remove the same author", () => {
       // First let's remove Author 1 by AJAX call (as if another user did this)
       cy.contains("tr", "Author 1").find(".btn .if-delete").click();
       cy.ensureModal("Confirm deletion")
@@ -35,12 +80,16 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
       cy.contains("tr", "Author 1").find(".btn .if-delete").click();
       cy.ensureModal("Confirm deletion").closeModal("Delete");
 
+      cy.verifyConflictErrorDialog("publication").closeModal("Close");
+
+      cy.reload();
+
       verifyContributors("#contributors-author-table", ["Author 2"]);
     });
   });
 
   describe("Publication editors", () => {
-    it("should not remove two editors when two users simultaneously remove the same editor", () => {
+    beforeEach(() => {
       cy.setUpPublication();
       cy.addEditor("Editor", "1", { external: true });
       cy.addEditor("Editor", "2", { external: true });
@@ -52,7 +101,52 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
         "Editor 1",
         "Editor 2",
       ]);
+    });
 
+    it("should remove the correct editor when reordered by another user", () => {
+      // Perform the reorder via AJAX
+      cy.get("#contributors-editor-table tbody")
+        .invoke("attr", "hx-headers")
+        .then(JSON.parse)
+        .then((htmxValues) => {
+          cy.get<string>("@biblioId").then((biblioId) => {
+            // Reverse the editors
+            const form = new URLSearchParams();
+            form.append("position", "1");
+            form.append("position", "0");
+
+            cy.htmxRequest({
+              method: "POST",
+              url: `/publication/${biblioId}/contributors/editor/order`,
+              headers: htmxValues,
+              form: true,
+              body: form.toString(),
+              log: true,
+            });
+          });
+        });
+
+      // Page is not refreshed so we still see the original order
+      verifyContributors("#contributors-editor-table", [
+        "Editor 1",
+        "Editor 2",
+      ]);
+
+      // Now let's try to delete Editor 1
+      cy.contains("tr", "Editor 1").find(".btn .if-delete").click();
+      cy.ensureModal("Confirm deletion").closeModal("Delete");
+
+      cy.verifyConflictErrorDialog("publication").closeModal("Close");
+
+      cy.reload();
+
+      verifyContributors("#contributors-editor-table", [
+        "Editor 2",
+        "Editor 1",
+      ]);
+    });
+
+    it("should not remove two editors when two users simultaneously remove the same editor", () => {
       // First let's remove Editor 1 by AJAX call (as if another user did this)
       cy.contains("tr", "Editor 1").find(".btn .if-delete").click();
       cy.ensureModal("Confirm deletion")
@@ -70,7 +164,7 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
   });
 
   describe("Publication supervisors", () => {
-    it("should not remove two supervisors when two users simultaneously remove the same supervisor", () => {
+    beforeEach(() => {
       cy.setUpPublication("Dissertation");
       cy.addSupervisor("Supervisor", "1", { external: true });
       cy.addSupervisor("Supervisor", "2", { external: true });
@@ -82,7 +176,52 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
         "Supervisor 1",
         "Supervisor 2",
       ]);
+    });
 
+    it("should remove the correct supervisor when reordered by another user", () => {
+      // Perform the reorder via AJAX
+      cy.get("#contributors-supervisor-table tbody")
+        .invoke("attr", "hx-headers")
+        .then(JSON.parse)
+        .then((htmxValues) => {
+          cy.get<string>("@biblioId").then((biblioId) => {
+            // Reverse the supervisors
+            const form = new URLSearchParams();
+            form.append("position", "1");
+            form.append("position", "0");
+
+            cy.htmxRequest({
+              method: "POST",
+              url: `/publication/${biblioId}/contributors/supervisor/order`,
+              headers: htmxValues,
+              form: true,
+              body: form.toString(),
+              log: true,
+            });
+          });
+        });
+
+      // Page is not refreshed so we still see the original order
+      verifyContributors("#contributors-supervisor-table", [
+        "Supervisor 1",
+        "Supervisor 2",
+      ]);
+
+      // Now let's try to delete Supervisor 1
+      cy.contains("tr", "Supervisor 1").find(".btn .if-delete").click();
+      cy.ensureModal("Confirm deletion").closeModal("Delete");
+
+      cy.verifyConflictErrorDialog("publication").closeModal("Close");
+
+      cy.reload();
+
+      verifyContributors("#contributors-supervisor-table", [
+        "Supervisor 2",
+        "Supervisor 1",
+      ]);
+    });
+
+    it("should not remove two supervisors when two users simultaneously remove the same supervisor", () => {
       // First let's remove Supervisor 1 by AJAX call (as if another user did this)
       cy.contains("tr", "Supervisor 1").find(".btn .if-delete").click();
       cy.ensureModal("Confirm deletion")
@@ -100,7 +239,7 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
   });
 
   describe("Dataset creators", () => {
-    it("should not remove two creators when two users simultaneously remove the same creator", () => {
+    beforeEach(() => {
       cy.setUpDataset();
       cy.addCreator("Creator", "1", { external: true });
       cy.addCreator("Creator", "2", { external: true });
@@ -112,7 +251,52 @@ describe("Issue #1701: Fix contributor removal concurrency bug", () => {
         "Creator 1",
         "Creator 2",
       ]);
+    });
 
+    it("should remove the correct creator when reordered by another user", () => {
+      // Perform the reorder via AJAX
+      cy.get("#contributors-author-table tbody")
+        .invoke("attr", "hx-headers")
+        .then(JSON.parse)
+        .then((htmxValues) => {
+          cy.get<string>("@biblioId").then((biblioId) => {
+            // Reverse the authors
+            const form = new URLSearchParams();
+            form.append("position", "1");
+            form.append("position", "0");
+
+            cy.htmxRequest({
+              method: "POST",
+              url: `/dataset/${biblioId}/contributors/author/order`,
+              headers: htmxValues,
+              form: true,
+              body: form.toString(),
+              log: true,
+            });
+          });
+        });
+
+      // Page is not refreshed so we still see the original order
+      verifyContributors("#contributors-author-table", [
+        "Creator 1",
+        "Creator 2",
+      ]);
+
+      // Now let's try to delete Creator 1
+      cy.contains("tr", "Creator 1").find(".btn .if-delete").click();
+      cy.ensureModal("Confirm deletion").closeModal("Delete");
+
+      cy.verifyConflictErrorDialog("publication").closeModal("Close");
+
+      cy.reload();
+
+      verifyContributors("#contributors-author-table", [
+        "Creator 2",
+        "Creator 1",
+      ]);
+    });
+
+    it("should not remove two creators when two users simultaneously remove the same creator", () => {
       // First let's remove Creator 1 by AJAX call (as if another user did this)
       cy.contains("tr", "Creator 1").find(".btn .if-delete").click();
       cy.ensureModal("Confirm deletion")
